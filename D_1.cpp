3827849606e1d55e2d3d343811a55630efac776e
firasel <mdrasel601390@gmail.com>
2022-07-04 23:14:02 +0600

    basic syntax practice

--- D_1.cpp
+++ D_1.cpp
@@ -1,9 +1,53 @@
-#include<iostream>
-#include<iomanip>
+#include<bits/stdc++.h>
+
 using namespace std;
+
+namespace one
+{
+    void fun()
+    {
+        cout<<"Hello world 1"<<endl;
+    }
+}
+
+namespace two
+{
+    void fun()
+    {
+        cout<<"Hello world 2"<<endl;
+    }
+}
+
 int main()
 {
-    double a=10.236598;
-    cout<<fixed<<setprecision(2)<<a<<endl;
+    char a[15]="Hello world";
+    cout<<a<<endl;
+
+    int b,c;
+    cin>>b>>c;
+    cout<<b<<' '<<c<<endl;
+
+    char str[50];
+    cin>>str;
+    cout<<str<<endl;
+
+    char str2[50];
+    cin.getline(str2,50);
+    cout<<str2<<endl;
+
+    int num[5];
+    for(int i=0; i<5; i++)
+        cin>>num[i];
+    for(int i=0; i<5; i++)
+        cout<<num[i]<<' ';
+    cout<<endl;
+
+    double e=11.2365;
+    int f=int(e);
+    cout<<f<<endl;
+
+    one::fun();
+    two::fun();
+
     return 0;
 }
