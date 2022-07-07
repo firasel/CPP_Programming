bb58e69f2ef4423ad3d40a92ecc859da6db8b675
firasel <mdrasel601390@gmail.com>
2022-07-07 12:22:50 +0600

    swap function practice

---
+++ D_4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=100;
+    int b=200;
+    cout<<a<<' '<<b<<endl;
+    swap(a,b);
+    cout<<a<<' '<<b<<endl;
+
+    return 0;
+}
