bdd72075eb3354c1ee581f96f6f6ad9b817f1bb9
firasel <mdrasel601390@gmail.com>
2022-07-08 17:23:27 +0600

    min max function

--- D_4.cpp
+++ D_4.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main(){
     int a=100;
     int b=200;
+    int minValue = min(a,b);
+    int maxValue = max(a,b);
+    cout<<"Min: "<<minValue<<endl<<"Max: "<<maxValue<<endl;
     cout<<a<<' '<<b<<endl;
     swap(a,b);
     cout<<a<<' '<<b<<endl;
