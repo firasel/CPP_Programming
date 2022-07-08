d1f051dba14f2f3c74be8a35c21698a2a37d8034
firasel <mdrasel601390@gmail.com>
2022-07-08 18:44:37 +0600

    practice problem e & f solved

--- D_5.cpp
+++ D_5.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char str[101];
-    cin>>str;
-    cout<<str[0]<<strlen(str)-2<<str[strlen(str)-1]<<endl;
+    int n,k,total=0;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    sort(arr,arr+n,std::greater<int>());
+    for(int i=0; i<k; i++)
+        total+=arr[i];
+    cout<<total<<endl;
     return 0;
 }
