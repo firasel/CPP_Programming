ce52d366ac0092f50207dc9666ecf7bbc8322073
firasel <mdrasel601390@gmail.com>
2022-07-08 18:57:18 +0600

    practice problem g & h solved

--- D_5.cpp
+++ D_5.cpp
@@ -1,14 +1,18 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string.h>
 using namespace std;
 int main(){
-    int n,k,total=0;
-    cin>>n>>k;
-    int arr[n];
-    for(int i=0; i<n; i++)
-        cin>>arr[i];
-    sort(arr,arr+n,std::greater<int>());
-    for(int i=0; i<k; i++)
-        total+=arr[i];
-    cout<<total<<endl;
+    char str[27];
+    cin>>str;
+    int arr[26]={0};
+    for(int i=0; i<strlen(str); i++)
+        arr[str[i]-'a']++;
+    for(int i=0; i<26; i++){
+        if(arr[i]>1){
+            cout<<"no"<<endl;
+            return 0;
+        }
+    }
+    cout<<"yes"<<endl;
     return 0;
 }
