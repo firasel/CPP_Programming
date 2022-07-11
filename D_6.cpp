e2d31e3ac34fbfa746488b387051045fe702ebc0
firasel <mdrasel601390@gmail.com>
2022-07-11 20:52:49 +0600

    Diagonal and scalar matrix check

--- D_6.cpp
+++ D_6.cpp
@@ -10,9 +10,36 @@ int main(){
         for(int j=0; j<col; j++)
             cin>>a[i][j];
 
-    int req_row;
-    cin>>req_row;
-    for(int i=0; i<col; i++)
-        cout<<a[req_row][i]<<' ';
+    if(row!=col){
+        cout<<"No"<<endl;
+        return 0;
+    }else{
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(i==j) continue;
+                if(a[i][j]>0){
+                    cout<<"No"<<endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    int temp;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(i==j){
+                if(i==0 && j==0) temp = a[i][j];
+                else{
+                    if(temp != a[i][j]){
+                        cout<<"Not scalar"<<endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<"Scalar"<<endl;
+
     return 0;
 }
