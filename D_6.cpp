3478b05c3157f001000a7ab523105f91ac497cfc
firasel <mdrasel601390@gmail.com>
2022-07-11 20:59:06 +0600

    Symmetric matrix check

--- D_6.cpp
+++ D_6.cpp
@@ -10,36 +10,15 @@ int main(){
         for(int j=0; j<col; j++)
             cin>>a[i][j];
 
-    if(row!=col){
-        cout<<"No"<<endl;
-        return 0;
-    }else{
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(i==j) continue;
-                if(a[i][j]>0){
-                    cout<<"No"<<endl;
-                    return 0;
-                }
-            }
-        }
-    }
-    int temp;
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
-            if(i==j){
-                if(i==0 && j==0) temp = a[i][j];
-                else{
-                    if(temp != a[i][j]){
-                        cout<<"Not scalar"<<endl;
-                        return 0;
-                    }
-                }
+            if(a[i][j] != a[j][i]){
+                cout<<"No"<<endl;
+                return 0;
             }
         }
     }
 
-    cout<<"Scalar"<<endl;
-
+    cout<<"Yes"<<endl;
     return 0;
 }
