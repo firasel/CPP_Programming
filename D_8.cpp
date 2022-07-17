a885c2ced4a726c365e5ddf59419b722c0884f28
firasel <mdrasel601390@gmail.com>
2022-07-17 23:10:27 +0600

    string modifiers in cpp

--- D_8.cpp
+++ D_8.cpp
@@ -4,13 +4,23 @@ using namespace std;
 int main()
 {
     string st="Hello world";
-    cout<<st.length()<<endl;
-    cout<<st.size()<<endl;
-    cout<<st<<' '<<st.size()<<endl;
-    st.resize(5);
-    cout<<st<<' '<<st.size()<<endl;
-    st.clear();
-    cout<<st<<' '<<st.size()<<endl;
-    st.empty() ? cout<<"Yes"<<endl : cout<<"No"<<endl;
+    cout<<st.front()<<' '<<st.at(6)<<' '<<st.back()<<endl;
+    st.assign("Hi");
+    cout<<st<<endl;
+
+    string st1="Hello";
+    string st2="World";
+    st1.append(st2);
+    st1.push_back('A');
+    st1.push_back('B');
+    st1.pop_back();
+    st1.insert(5," W ");
+    st1.erase(6,2);
+    cout<<st1<<endl;
+
+    string st3="First";
+    string st4="Second";
+    swap(st3,st4);
+    cout<<st3<<' '<<st4<<endl;
     return 0;
 }
