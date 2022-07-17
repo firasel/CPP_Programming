b06a604d8976c2a1b85115b87e4aad0bf554609b
firasel <mdrasel601390@gmail.com>
2022-07-17 23:26:38 +0600

    string streams in cpp

--- D_8.cpp
+++ D_8.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main()
 {
-    string st="Hello world";
-    cout<<st.find('l')<<' '<<st.find_first_of('o')<<' '<<st.find_last_of('o')<<endl;
-    cout<<st.find_first_not_of('H')<<' '<<st.find_last_not_of('d')<<endl;
-    cout<<st.substr(2,8)<<endl;
+    string st="ab   ab cd   cd xy xy";
+    stringstream stream(st);
+
+    string w;
+    while(stream>>w)
+    {
+        cout<<w<<endl;
+    }
 
     return 0;
 }
