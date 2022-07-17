b2d006d9aa90bf73415f2b0296c5f58a760cee09
firasel <mdrasel601390@gmail.com>
2022-07-17 23:22:34 +0600

    string operations in cpp

--- D_8.cpp
+++ D_8.cpp
@@ -4,23 +4,9 @@ using namespace std;
 int main()
 {
     string st="Hello world";
-    cout<<st.front()<<' '<<st.at(6)<<' '<<st.back()<<endl;
-    st.assign("Hi");
-    cout<<st<<endl;
+    cout<<st.find('l')<<' '<<st.find_first_of('o')<<' '<<st.find_last_of('o')<<endl;
+    cout<<st.find_first_not_of('H')<<' '<<st.find_last_not_of('d')<<endl;
+    cout<<st.substr(2,8)<<endl;
 
-    string st1="Hello";
-    string st2="World";
-    st1.append(st2);
-    st1.push_back('A');
-    st1.push_back('B');
-    st1.pop_back();
-    st1.insert(5," W ");
-    st1.erase(6,2);
-    cout<<st1<<endl;
-
-    string st3="First";
-    string st4="Second";
-    swap(st3,st4);
-    cout<<st3<<' '<<st4<<endl;
     return 0;
 }
