e5b058e0a5bb5ac7c1017e9bea46e074a3f23be1
firasel <mdrasel601390@gmail.com>
2022-07-18 23:45:25 +0600

    Operator overloading return object

--- D_9.cpp
+++ D_9.cpp
@@ -11,13 +11,17 @@ public:
     }
     Example operator +(Example obj)
     {
-        cout<<x+obj.x<<endl;
+        Example ans(0);
+        ans.x=x+obj.x;
+        return ans;
     }
 };
 
 int main()
 {
-    Example a(10), b(20);
-    a+b;
+    Example a(10), b(20), c(50);
+    Example ans=a+b;
+    Example ans2=ans+c;
+    cout<<ans2.x<<endl;
     return 0;
 }
