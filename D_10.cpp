a6c9cd3fb52c6a2d91756c77c19f2e5ecd2043e0
firasel <mdrasel601390@gmail.com>
2022-07-20 00:54:09 +0600

    account create with class

--- D_10.cpp
+++ D_10.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class BankAccount
 {
 public:
-    string accout_holder;
+    string account_holder;
     string address;
     int age;
     int account_number;
@@ -12,19 +12,44 @@ protected:
     int balance;
 private:
     string password;
-    BankAccount(string accout_holder, string address, int age, string password)
+public:
+    BankAccount(string account_holder, string address, int age, string password)
     {
-        this -> accout_holder = accout_holder;
+        this -> account_holder = account_holder;
         this -> address = address;
         this -> age = age;
         this -> password = password;
-        this -> account_number = rend()%1000000000;
+        this -> account_number = rand()%1000000000;
+        this -> balance = 0;
         cout<<"Your account no is "<<this->account_number<<endl;
+    }
+    int show_balance(string password)
+    {
+        if(this -> password == password)
+        {
+            return this -> balance;
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
 
+BankAccount* create_account()
+{
+    string account_holder, password, address;
+    int age;
+    cout<<"Create Account"<<endl;
+    cin>>account_holder>>address>>age>>password;
+    BankAccount *myAccount = new BankAccount(account_holder,address,age,password);
+    return myAccount;
+}
+
 int main()
 {
-
+    BankAccount *myAccount = create_account();
+    if(myAccount->show_balance("abcd") == -1) cout<<"Password didn't match"<<endl;
+    else cout<<"Your bank balance is "<<myAccount->show_balance("abcd")<<endl;
     return 0;
 }
