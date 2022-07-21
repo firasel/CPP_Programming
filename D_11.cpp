18157b0c07dad6e6ff04278c52d7a26440f90754
firasel <mdrasel601390@gmail.com>
2022-07-22 00:22:55 +0600

    Practice problem solution modify

--- D_11.cpp
+++ D_11.cpp
@@ -4,9 +4,9 @@ using namespace std;
 class Shop
 {
 public:
-    string product_name;
-    int product_price;
-    int product_quantity;
+    string product_name[10];
+    int product_price[10];
+    int product_quantity[10];
 private:
     int total_income;
 public:
@@ -22,53 +22,57 @@ public:
     {
         this -> total_income = 0;
     }
-};
-
-void takeInput(int n, Shop products[])
-{
-    for(int i=0; i<n; i++)
+    // Take product input
+    void takeInput(int n)
     {
-        cout<<"Product "<<i+1<<" Name: ";
-        cin>>products[i].product_name;
-        cout<<"Product "<<i+1<<" Price: ";
-        cin>>products[i].product_price;
-        cout<<"Product "<<i+1<<" Quantity: ";
-        cin>>products[i].product_quantity;
+        for(int i=0; i<n; i++)
+        {
+            cout<<"Product "<<i+1<<" Name: ";
+            cin>>product_name[i];
+            cout<<"Product "<<i+1<<" Price: ";
+            cin>>product_price[i];
+            cout<<"Product "<<i+1<<" Quantity: ";
+            cin>>product_quantity[i];
+        }
+    }
+    // Print all product details
+    void printProducts(int n)
+    {
+        cout<<endl<<endl<<"        BUY PRODUCTS        "<<endl;
+        cout<<"        ____________        "<<endl<<endl;
+        cout<<"Product Index:    ";
+        for(int i=1; i<=n; i++) cout<<i<<"        ";
+        cout<<endl;
+        cout<<"Product Name:     ";
+        for(int i=0; i<n; i++) cout<<product_name[i]<<"   ";
+        cout<<endl;
+        cout<<"Product Price:    ";
+        for(int i=0; i<n; i++) cout<<product_price[i]<<"   ";
+        cout<<endl;
+        cout<<"Product Quantity: ";
+        for(int i=0; i<n; i++) cout<<product_quantity[i]<<"   ";
+        cout<<endl<<endl;
+    }
+    // Buy product method
+    void buyProduct(int n)
+    {
+        int select,qty;
+        cout<<"Which product do you want? From 1 to "<<n<<": ";
+        cin>>select;
+        select -= 1;
+        cout<<"What is the quantity that you want of "<<product_name[select]<<": ";
+        cin>>qty;
+        cout<<endl;
+        if(qty <= product_quantity[select])
+        {
+            product_quantity[select] -= qty;
+            cout<<product_name[select]<<" bought "<<qty<<" pc successfully"<<endl;
+            this->setter(product_price[select] * qty);
+            cout<<"Your total income : "<<this->getter()<<endl;
+        }
+        else cout<<"This quantity is not available"<<endl;
     }
 };
-
-void printProducts(int n, Shop products[]){
-    cout<<endl<<endl<<"        BUY PRODUCTS        "<<endl;
-    cout<<"        ____________        "<<endl<<endl;
-    cout<<"Product Index:    ";
-    for(int i=1; i<=n; i++) cout<<i<<"        ";
-    cout<<endl;
-    cout<<"Product Name:     ";
-    for(int i=0; i<n; i++) cout<<products[i].product_name<<"   ";
-    cout<<endl;
-    cout<<"Product Price:    ";
-    for(int i=0; i<n; i++) cout<<products[i].product_price<<"   ";
-    cout<<endl;
-    cout<<"Product Quantity: ";
-    for(int i=0; i<n; i++) cout<<products[i].product_quantity<<"   ";
-    cout<<endl<<endl;
-}
-
-void buyProduct(int n, Shop products[]){
-    int select,qty;
-    cout<<"Which product do you want? From 1 to "<<n<<": ";
-    cin>>select;
-    select -= 1;
-    cout<<"What is the quantity that you want of "<<products[select].product_name<<": ";
-    cin>>qty;
-    cout<<endl;
-    if(qty <= products[select].product_quantity){
-        products[select].product_quantity -= qty;
-        cout<<products[select].product_name<<" bought "<<qty<<" pc successfully"<<endl;
-        products[select].setter(products[select].product_price * qty);
-        cout<<"Your total income : "<<products[select].getter()<<endl;
-    }else cout<<"This quantity is not available"<<endl;
-}
 
 int main()
 {
@@ -76,11 +80,13 @@ int main()
     cout<<"Number of products: ";
     cin>>n;
 
-    Shop products[n];
-    takeInput(n, products);
-    while(true){
-        printProducts(n, products);
-        buyProduct(n, products);
+    Shop products;
+    products.takeInput(n);
+
+    while(true)
+    {
+        products.printProducts(n);
+        products.buyProduct(n);
     }
     return 0;
 }
