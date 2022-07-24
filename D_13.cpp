dfaf9ab8b991110861d2818c997aca8c897fc501
firasel <mdrasel601390@gmail.com>
2022-07-24 21:29:43 +0600

    array value insertion in cpp

--- D_13.cpp
+++ D_13.cpp
@@ -22,6 +22,28 @@ int main()
     cout<<"Given Array: ";
     PrintArray(array,size);
 
+    int pos,value;
+    cout<<"Position of the Insertion: ";
+    cin>>pos;
+    cout<<"Value of the position: ";
+    cin>>value;
+
+    if(pos<0 || pos>size)
+    {
+        cout<<"Invalid Index";
+    }
+    else
+    {
+        for(int i=size-1; i>=pos; i--)
+        {
+            array[i+1] = array[i];
+        }
+        array[pos] = value;
+    }
+
+    cout<<"Array After the Insertion : ";
+    PrintArray(array, size+1);
+
     return 0;
 }
 
