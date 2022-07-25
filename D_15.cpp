556d4f84e867858859f9ae6fd29b90b97b62c764
firasel <mdrasel601390@gmail.com>
2022-07-26 00:32:37 +0600

    practice problem 6 solved

--- D_15.cpp
+++ D_15.cpp
@@ -3,31 +3,36 @@ using namespace std;
 
 int main()
 {
-    int size;
-    cout<<"Enter the array size: ";
-    cin>>size;
+    int size1, size2;
+    cout<<"Enter the first array size: ";
+    cin>>size1;
+    int array1[size1];
 
-    int array[size];
+    for(int i=0; i<size1; i++) cin>>array1[i];
 
-    for(int i=0; i<size; i++)
-        cin>>array[i];
-    int checkValue;
-    cin>>checkValue;
+    cout<<"Enter the second array size: ";
+    cin>>size2;
+    int array2[size2];
 
-    int findPos[size],index=0,found=0;
-    for(int i=0; i<size; i++){
-        if(array[i]==checkValue){
-            found=1;
-            findPos[index] = i;
-            index++;
+    for(int i=0; i<size2; i++) cin>>array2[i];
+
+
+    int findPos[size1>size2 ? size1 : size2],index=0,found=0;
+    for(int i=0; i<size1; i++){
+        for(int j=0; j<size2; j++){
+            if(array1[i]==array2[j]){
+                found=1;
+                findPos[index] = array1[i];
+                index++;
+            }
         }
     }
 
-    if(found==0) cout<<"NOT FOUND"<<endl;
+    if(found==0) cout<<"Empty set"<<endl;
     else{
-        cout<<"FOUND at index position: ";
         for(int i=0; i<index; i++)
             cout<<findPos[i]<<" ";
+        cout<<endl;
     }
 
     return 0;
