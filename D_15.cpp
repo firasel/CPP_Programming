8b4a2713a23caa90f28e968c401391ad92521598
firasel <mdrasel601390@gmail.com>
2022-07-26 00:44:47 +0600

    all practice problem solved

--- D_15.cpp
+++ D_15.cpp
@@ -8,17 +8,20 @@ int main()
     cin>>size1;
     int array1[size1];
 
-    for(int i=0; i<size1; i++)
-    {
-        cin>>array1[i];
-        if(array1[i]%3==0)
-        {
-            array1[i] = -1;
+    for(int i=0; i<size1; i++) cin>>array1[i];
+
+    int array2[size1], index=0;
+    for(int i=0; i<size1; i++){
+        int found = 0;
+        for(int j=0; j<index; j++)
+            if(array1[i]==array2[j]) found = 1;
+        if(found == 0){
+            array2[index] = array1[i];
+            index++;
         }
     }
 
-    for(int i=0; i<size1; i++)
-        cout<<array1[i]<<" ";
+    for(int i=0; i<index; i++) cout<<array2[i]<<" ";
     cout<<endl;
 
     return 0;
