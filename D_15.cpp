49ae2187c0e333bffccf7a4e527347e9fbcdae0e
firasel <mdrasel601390@gmail.com>
2022-07-26 00:36:04 +0600

    practice problem 7 solved

--- D_15.cpp
+++ D_15.cpp
@@ -3,37 +3,23 @@ using namespace std;
 
 int main()
 {
-    int size1, size2;
-    cout<<"Enter the first array size: ";
+    int size1;
+    cout<<"Enter the array size: ";
     cin>>size1;
     int array1[size1];
 
-    for(int i=0; i<size1; i++) cin>>array1[i];
-
-    cout<<"Enter the second array size: ";
-    cin>>size2;
-    int array2[size2];
-
-    for(int i=0; i<size2; i++) cin>>array2[i];
-
-
-    int findPos[size1>size2 ? size1 : size2],index=0,found=0;
-    for(int i=0; i<size1; i++){
-        for(int j=0; j<size2; j++){
-            if(array1[i]==array2[j]){
-                found=1;
-                findPos[index] = array1[i];
-                index++;
-            }
+    for(int i=0; i<size1; i++)
+    {
+        cin>>array1[i];
+        if(array1[i]%3==0)
+        {
+            array1[i] = -1;
         }
     }
 
-    if(found==0) cout<<"Empty set"<<endl;
-    else{
-        for(int i=0; i<index; i++)
-            cout<<findPos[i]<<" ";
-        cout<<endl;
-    }
+    for(int i=0; i<size1; i++)
+        cout<<array1[i]<<" ";
+    cout<<endl;
 
     return 0;
 }
