52e5468a58b3bbef15d4492437a816b189d6eed6
firasel <mdrasel601390@gmail.com>
2022-07-27 02:30:52 +0600

    bubble sort optimize

--- D_16.cpp
+++ D_16.cpp
@@ -19,14 +19,17 @@ int main()
     cout<<"Before Sort: ";
     printArray(array,size);
     // Bubble Sort Implementation
-    for(int i=0; i<size-1; i++){
-        for(int j=0; j<size-1; j++){
+    for(int i=1; i<size; i++){
+        int flag = 0;
+        for(int j=0; j<size-i; j++){
             if(array[j]>array[j+1]){
                 int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
+                flag = 1;
             }
         }
+        if(flag == 0) break;
     }
 
     cout<<"After Sort: ";
