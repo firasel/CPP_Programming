bf715f67676ba5d2422bffc04a83a014bc9ee902
firasel <mdrasel601390@gmail.com>
2022-07-29 03:49:29 +0600

    contest problem f to h solved

--- D_17.cpp
+++ D_17.cpp
@@ -1,29 +1,45 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin>>t;
-
-    for(int i=0; i<t; i++){
+    int tc;
+    std::ios::sync_with_stdio(false);
+    cin>>tc;
+    for(int t=0; t<tc; t++)
+    {
         int n,q;
-        cin.ignore();
         cin>>n>>q;
         int arr[n];
-        for(int j=0; j<n; j++) cin>>arr[j];
-        cout<<"Case "<<i+1<<":"<<"\n";
-        for(int j=0; j<q; j++){
-            int a,b;
-            cin>>a>>b;
-            a--;
-            b--;
-            int minNum = arr[a];
-            for(int k=a+1; k<=b; k++)
-                if(arr[k]<minNum) minNum = arr[k];
-            cout<<minNum<<"\n";
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        cout<<"Case "<<t+1<<":\n";
+        for(int i=0; i<q; i++)
+        {
+            int k;
+            cin>>k;
+            if(k==1)
+            {
+                int p;
+                cin>>p;
+                cout<<arr[p]<<"\n";
+                arr[p]=0;
+            }
+            else if(k==2)
+            {
+                int p,m;
+                cin>>p>>m;
+                arr[p]+=m;
+            }
+            else if(k==3)
+            {
+                int s,e,sum=0;
+                cin>>s>>e;
+                for(s; s<=e; s++) sum+=arr[s];
+                cout<<sum<<"\n";
+            }
         }
     }
+
     return 0;
 }
