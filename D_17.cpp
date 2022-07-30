5f8df909015c033eef9cffb33fbd2f71e0b6d53b
firasel <mdrasel601390@gmail.com>
2022-07-30 16:37:56 +0600

    All contest problem solved

--- D_17.cpp
+++ D_17.cpp
@@ -1,39 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    int tc;
+int main()
+{
     std::ios::sync_with_stdio(false);
-    cin>>tc;
-    for(int t=0; t<tc; t++){
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int i=1; i<=t; i++){
         int n,q;
+        cin.ignore();
         cin>>n>>q;
         int arr[n];
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        cout<<"Case "<<t+1<<":\n";
-        for(int i=0;i<q;i++){
-            int k;
-            cin>>k;
-            if(k==1){
-                int p;
-                cin>>p;
-                cout<<arr[p]<<"\n";
-                arr[p]=0;
-            }else if(k==2){
-                int p,m;
-                cin>>p>>m;
-                arr[p]+=m;
-            }else if(k==3){
-                int s,e,sum=0;
-                cin>>s>>e;
-                for(s;s<=e;s++) sum+=arr[s];
-                cout<<sum<<"\n";
-            }
+        for(int j=0; j<n; j++) cin>>arr[j];
+        cout<<"Case "<<i<<":\n";
+        for(int j=0; j<q; j++){
+            int a,b;
+            cin>>a>>b;
+            a--;
+            b--;
+            int minNum = arr[a];
+            for(int k=a+1; k<=b; k++)
+                if(arr[k]<minNum) minNum = arr[k];
+            cout<<minNum<<"\n";
         }
     }
-
     return 0;
 }
-
-
