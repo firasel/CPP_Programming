7dd22c718c6f2662a4d6cb9e2d5f26641ceb8832
firasel <mdrasel601390@gmail.com>
2022-08-04 01:58:36 +0600

    practice problem 5 solved

--- D_21.cpp
+++ D_21.cpp
@@ -13,14 +13,8 @@ int main()
 
     int sum=0;
     for(int i=0; i<n; i++)
-    {
         for(int j=0; j<n; j++)
-        {
-            if(i==0 || i==n-1) sum+=arr[i][j];
-            else if(i==n/2 && j==n/2) sum+=arr[i][j];
-            else if(i!=n/2 && (i==j || i==n-j-1)) sum+=arr[i][j];
-        }
-    }
+            if(i%2!=0 || j%2!=0) sum+=arr[i][j];
 
     cout<<sum<<endl;
     return 0;
