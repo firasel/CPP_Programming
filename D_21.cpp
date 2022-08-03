2c9cb223950d8f4d47a98182cacde2742075404a
firasel <mdrasel601390@gmail.com>
2022-08-04 01:34:23 +0600

    practice problem 2 solved

--- D_21.cpp
+++ D_21.cpp
@@ -3,19 +3,26 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    int sqrtValue = sqrt(n);
-    int arr[sqrtValue][sqrtValue];
+    int arr[3][3];
+    int arr2[3][3];
 
-    for(int i=0; i<sqrtValue; i++)
-        for(int j=0; j<sqrtValue; j++)
+    for(int i=0; i<3; i++)
+        for(int j=0; j<3; j++)
             cin>>arr[i][j];
 
-    for(int i=0; i<sqrtValue; i++)
+    for(int i=0; i<3; i++)
+        for(int j=0; j<3; j++)
+            cin>>arr2[i][j];
+
+    for(int i=0; i<3; i++)
     {
-        for(int j=0; j<sqrtValue; j++)
-            cout<<arr[i][j]<<" ";
+        for(int j=0; j<3; j++){
+            int sum=0;
+            for(int k=0; k<3; k++){
+                sum+=(arr[i][k]*arr2[k][j]);
+            }
+            cout<<sum<<" ";
+        }
         cout<<endl;
     }
 
