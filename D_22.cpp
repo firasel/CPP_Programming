333c477f941cda1f9013e249d0643cca3bdbcf3a
firasel <mdrasel601390@gmail.com>
2022-08-05 21:11:58 +0600

    Counting sort step show

--- D_22.cpp
+++ D_22.cpp
@@ -8,25 +8,50 @@ void printArr(int arr[], int n)
 
 int main()
 {
-    int arr[6] = {7, 2, 13, 2, 11, 4};
-    for(int i=1; i<=6; i++)
+    int n=8;
+    int arr[n] = {7, 2, 9, 2, 7, 4, 5, 1};
+    cout<<"Array: ";
+    printArr(arr,n);
+    cout<<endl;
+
+    // Step 1
+    int max=INT_MIN;
+    for(int i=0; i<n; i++)
+        if(arr[i] > max)
+            max = arr[i];
+
+    // Step 2
+    int count[max+1];
+    for(int i=0; i<=max; i++) count[i]=0;
+
+    // Step 3
+    for(int i=0; i<n; i++) count[arr[i]]++;
+    cout<<"Count: ";
+    printArr(count,max+1);
+    cout<<endl;
+    // Step 4
+    for(int i=1; i<=max; i++) count[i] += count[i-1];
+    cout<<"Count Sum: ";
+    printArr(count,max+1);
+    cout<<endl;
+
+    // Step 5
+    int final[n];
+    for(int i=0; i<n; i++) final[i]=0;
+
+    for(int i=n-1; i>=0; i--)
     {
-        cout<<i<<" Iteration:"<<endl;
-        for(int j=0; j<5; j++)
-        {
-            cout<<j+1<<" Step: ";
-            printArr(arr,6);
-            cout<<" -> ";
-            if(arr[j]>arr[j+1])
-            {
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-            printArr(arr,6);
-            cout<<endl;
-        }
+        printArr(final,n);
+        cout<<" -> ";
+        count[arr[i]]--;
+        int k = count[arr[i]];
+        final[k] = arr[i];
+        printArr(final,n);
+        cout<<endl;
     }
+
+    cout<<"After Sort: ";
+    printArr(final,n);
 
     return 0;
 }
