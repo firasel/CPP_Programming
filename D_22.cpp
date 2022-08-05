4d323806658c6551cf74f84a84bd8d823b620263
firasel <mdrasel601390@gmail.com>
2022-08-05 21:50:51 +0600

    Insertion sort step show

--- D_22.cpp
+++ D_22.cpp
@@ -8,50 +8,33 @@ void printArr(int arr[], int n)
 
 int main()
 {
-    int n=8;
-    int arr[n] = {7, 2, 9, 2, 7, 4, 5, 1};
+    int n=6;
+    int arr[n] = {5, 1, 3, 8, 2, 2};
     cout<<"Array: ";
     printArr(arr,n);
     cout<<endl;
 
-    // Step 1
-    int max=INT_MIN;
-    for(int i=0; i<n; i++)
-        if(arr[i] > max)
-            max = arr[i];
-
-    // Step 2
-    int count[max+1];
-    for(int i=0; i<=max; i++) count[i]=0;
-
-    // Step 3
-    for(int i=0; i<n; i++) count[arr[i]]++;
-    cout<<"Count: ";
-    printArr(count,max+1);
-    cout<<endl;
-    // Step 4
-    for(int i=1; i<=max; i++) count[i] += count[i-1];
-    cout<<"Count Sum: ";
-    printArr(count,max+1);
-    cout<<endl;
-
-    // Step 5
-    int final[n];
-    for(int i=0; i<n; i++) final[i]=0;
-
-    for(int i=n-1; i>=0; i--)
+    for(int i=1; i<n; i++)
     {
-        printArr(final,n);
-        cout<<" -> ";
-        count[arr[i]]--;
-        int k = count[arr[i]];
-        final[k] = arr[i];
-        printArr(final,n);
+        cout<<i<<" Step: ";
+        printArr(arr,n);
+        cout<<"-> ";
+        int key = arr[i];
+        int j = i-1;
+        while(arr[j]>key && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j--;
+            printArr(arr,n);
+            cout<<"-> ";
+        }
+        arr[j+1] = key;
+        printArr(arr,n);
         cout<<endl;
     }
 
     cout<<"After Sort: ";
-    printArr(final,n);
+    printArr(arr,n);
 
     return 0;
 }
