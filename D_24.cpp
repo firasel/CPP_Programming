bd7ee451f62190611035b2af805473955649f171
firasel <mdrasel601390@gmail.com>
2022-08-09 18:18:59 +0600

    Linked list class declare

---
+++ D_24.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *Next;
+    Node(int val)
+    {
+        value = val;
+    }
+};
+
+int main()
+{
+    NOde* head = new Node(1);
+
+    return 0;
+}
+
