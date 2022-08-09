c65438c39c9ae7cbf3540d5f7d604fb8a8f1a6a4
firasel <mdrasel601390@gmail.com>
2022-08-09 18:27:14 +0600

    Linked list print function implemented

--- D_24.cpp
+++ D_24.cpp
@@ -9,12 +9,36 @@ public:
     Node(int val)
     {
         value = val;
+        Next = NULL;
     }
 };
 
+void displayList(Node *&head);
+
+void displayList(Node *&head)
+{
+    Node *temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->value<<" ";
+        temp=temp->Next;
+    }
+    cout<<endl;
+}
+
 int main()
 {
-    NOde* head = new Node(1);
+    Node *head = new Node(1);
+    Node *first = new Node(2);
+    Node *second = new Node(3);
+    Node *third = new Node(4);
+
+    head->Next = first;
+    first->Next = second;
+    second->Next = third;
+
+    cout<<"Linked List: ";
+    displayList(head);
 
     return 0;
 }
