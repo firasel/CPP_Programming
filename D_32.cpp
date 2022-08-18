435cd1734419a4974139c4b8e95f50c11f7ac9c7
firasel <mdrasel601390@gmail.com>
2022-08-18 23:23:52 +0600

    Lab exam problem 1 & 2 solved

--- D_32.cpp
+++ D_32.cpp
@@ -3,11 +3,42 @@ using namespace std;
 
 int main()
 {
-    int a,b,c=0;
-    cin>>a>>b;
-    if(a%2==1 || b%2==1) c=((b-a)/2)+1;
-    else c=(b-a)/2;
-    cout<<c<<endl;
+    int n,m;
+
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    cin>>m;
+    int arr2[m];
+    for(int i=0; i<m; i++) cin>>arr2[i];
+
+    int size = m+n;
+    int finalArr[size];
+    fill(finalArr,finalArr+size,0);
+    int k=0;
+
+    for(int i=0; i<n; i++)
+    {
+        int flag=0;
+        for(int j=0; j<m; j++)
+        {
+            if(arr[i]==arr2[j])
+            {
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==0)
+        {
+            finalArr[k] = arr[i];
+            k++;
+        }
+    }
+
+    for(int i=0; i<k; i++) cout<<finalArr[i]<<" ";
+
     return 0;
 }
 
