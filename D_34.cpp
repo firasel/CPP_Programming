257a1f86cbc2c56778767372c0301375b3083161
firasel <mdrasel601390@gmail.com>
2022-08-25 11:02:33 +0600

    Practice problem 6 solved

--- D_34.cpp
+++ D_34.cpp
@@ -1,40 +1,50 @@
 #include<bits/stdc++.h>
-#include"D_30.h"
 
 using namespace std;
 
 int main()
 {
-    Stack <int> st;
-    Stack <int> minSt;
-    int n, min=INT_MAX;
-    cin>>n;
-    int array[n];
-    for(int i=0; i<n; i++) cin>>array[i];
-
+    stack <int> st;
+    queue <int> qu;
+    int n,k;
+    cin>>n>>k;
     for(int i=0; i<n; i++)
     {
-        if(array[i]<=min)
+        int val;
+        cin>>val;
+        qu.push(val);
+    };
+
+    for(int i=0; i<n/k; i++)
+    {
+        for(int j=0; j<k; j++)
         {
-            minSt.push(array[i]);
-            min=array[i];
+            st.push(qu.front());
+            qu.pop();
         }
-        st.push(array[i]);
+        while(!st.empty())
+        {
+            qu.push(st.top());
+            st.pop();
+        }
+    }
+
+    for(int j=0; j<n%k; j++)
+    {
+        st.push(qu.front());
+        qu.pop();
     }
 
     while(!st.empty())
     {
-        if(minSt.Top()!=st.Top())
-        {
-            st.pop();
-            cout<<minSt.Top()<<" ";
-        }
-        else
-        {
-            cout<<minSt.Top()<<" ";
-            minSt.pop();
-            st.pop();
-        }
+        qu.push(st.top());
+        st.pop();
+    }
+
+    while(!qu.empty())
+    {
+        cout<<qu.front()<<" ";
+        qu.pop();
     }
     cout<<endl;
 
