6195eb8cbc3546894bc3c8d06a5288d26b42679c
firasel <mdrasel601390@gmail.com>
2022-08-25 10:29:23 +0600

    Practice problem 8 solved

--- D_34.cpp
+++ D_34.cpp
@@ -3,45 +3,39 @@
 
 using namespace std;
 
-void insertElement(Stack <int> &st, int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        int val;
-        cin>>val;
-        st.push(val);
-    }
-}
-
-void stackSort(Stack <int> &st)
-{
-    Stack <int> temp;
-    while(!st.empty())
-    {
-        int tmp = st.Top();
-        st.pop();
-
-        while(!temp.empty() && temp.Top() > tmp)
-        {
-            st.push(temp.Top());
-            temp.pop();
-        }
-        temp.push(tmp);
-    }
-
-
-    st = temp;
-}
-
 int main()
 {
     Stack <int> st;
-    int n;
+    Stack <int> minSt;
+    int n, min=INT_MAX;
     cin>>n;
-    insertElement(st, n);
-    stackSort(st);
+    int array[n];
+    for(int i=0; i<n; i++) cin>>array[i];
 
-    while(!st.empty()) cout<<st.pop()<<" ";
+    for(int i=0; i<n; i++)
+    {
+        if(array[i]<=min)
+        {
+            minSt.push(array[i]);
+            min=array[i];
+        }
+        st.push(array[i]);
+    }
+
+    while(!st.empty())
+    {
+        if(minSt.Top()!=st.Top())
+        {
+            st.pop();
+            cout<<minSt.Top()<<" ";
+        }
+        else
+        {
+            cout<<minSt.Top()<<" ";
+            minSt.pop();
+            st.pop();
+        }
+    }
     cout<<endl;
 
     return 0;
