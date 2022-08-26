1d28f88526cc391ed1787a401ab31712b597b934
firasel <mdrasel601390@gmail.com>
2022-08-26 11:18:17 +0600

    Assignment problem 2 to 5 solved

--- D_35.cpp
+++ D_35.cpp
@@ -1,25 +1,78 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class doublyNode
+{
 public:
-    bool isPalindrome(int x) {
-        int num = x, rem=0;
-        double revNum=0;
-        while(num>0){
-            rem=num%10;
-            revNum=revNum*10+rem;
-            num/=10;
-        }
-        return revNum==x;
+    int Value;
+    doublyNode *Prev;
+    doublyNode *Next;
+
+    doublyNode(int val)
+    {
+        Value = val;
+        Prev = NULL;
+        Next = NULL;
     }
 };
 
-int main()
+void displayList(doublyNode* &head);
+void insertAtTail(doublyNode* &head, int val);
+void insertAtHead(doublyNode* &head, int val);
+
+
+void displayList(doublyNode* &head)
 {
-    Solution st;
-    if(st.isPalindrome(121)) cout<<"True"<<endl;
-    else cout<<"False"<<endl;
-    return 0;
+    doublyNode *temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->Value;
+        if(temp->Next != NULL) cout<<" -> ";
+        temp=temp->Next;
+    }
+    cout<<endl;
 }
 
+void insertAtTail(doublyNode* &head, int val)
+{
+    doublyNode *temp = head;
+    doublyNode *newNode = new doublyNode(val);
+    if(temp==NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    while(temp->Next!=NULL)
+    {
+        temp=temp->Next;
+    }
+    temp->Next = newNode;
+    newNode->Prev = temp;
+}
+
+void insertAtHead(doublyNode* &head, int val)
+{
+    doublyNode *newNode = new doublyNode(val);
+    head->Prev = newNode;
+    newNode->Next = head;
+    head = newNode;
+}
+
+int main()
+{
+    int choice, val, position;
+    doublyNode *head = NULL;
+
+    insertAtTail(head, 12);
+    insertAtTail(head, 16);
+    insertAtTail(head, 28);
+    insertAtTail(head, 25);
+    insertAtTail(head, 53);
+
+    displayList(head);
+    cout<<endl;
+    cout<<head->Next->Next->Value<<endl;
+
+    return 0;
+}
