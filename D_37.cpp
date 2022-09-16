8c2b853f9b27dac1c0c699ad15c87a31843e3c5e
firasel <mdrasel601390@gmail.com>
2022-09-16 23:19:41 +0600

    binary tree inorder, preorder, postorder traverse

--- D_37.cpp
+++ D_37.cpp
@@ -160,39 +160,158 @@ public:
     }
 };
 
+void inOrder(TreeNode *root, string &chk)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inOrder(root->left, chk);
+    chk += to_string(root->val) + " ";
+    inOrder(root->right, chk);
+}
+
+void preOrder(TreeNode *root, string &chk)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    chk += to_string(root->val) + " ";
+    preOrder(root->left, chk);
+    preOrder(root->right, chk);
+}
+
+void postOrder(TreeNode *root, string &chk)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left, chk);
+    postOrder(root->right, chk);
+    chk += to_string(root->val) + " ";
+}
+
+int levelOrderTraversal(TreeNode *root, string &chk, int k)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    int level = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    q.push(NULL);
+    int max = INT_MIN;
+
+    while (!q.empty())
+    {
+        TreeNode *chkNode = q.front();
+        q.pop();
+        if (chkNode != NULL)
+        {
+            if (level == k && max < chkNode->val)
+            {
+                max = chkNode->val;
+            }
+            chk += to_string(chkNode->val) + " ";
+            if (chkNode->left != NULL)
+            {
+                q.push(chkNode->left);
+            }
+
+            if (chkNode->right != NULL)
+            {
+                q.push(chkNode->right);
+            }
+        }
+        else
+        {
+            if (!q.empty())
+            {
+                q.push(NULL);
+                level++;
+            }
+        }
+    }
+    return max;
+}
+
+int maxDepth(TreeNode *node)
+{
+    if (node == NULL)
+        return -1;
+    else
+    {
+        /* compute the depth of each subtree */
+        int lDepth = maxDepth(node->left);
+        int rDepth = maxDepth(node->right);
+
+        /* use the larger one */
+        if (lDepth > rDepth)
+            return (lDepth + 1);
+        else
+            return (rDepth + 1);
+    }
+}
+
 int main()
 {
-    // int a;
-    // cin >> a;
-    // TreeNode *root = new TreeNode(a);
-    // queue<TreeNode *> q;
-    // q.push(root);
-    // while (!q.empty())
-    // {
-    //     TreeNode *presentRoot = q.front();
-    //     q.pop();
-    //     int x, y;
-    //     cin >> x >> y;
-    //     TreeNode *n1 = NULL;
-    //     TreeNode *n2 = NULL;
-    //     if (x != -1)
-    //         n1 = new TreeNode(x);
-    //     if (y != -1)
-    //         n2 = new TreeNode(y);
-    //     presentRoot->left = n1;
-    //     presentRoot->right = n2;
-    //     if (n1 != NULL)
-    //         q.push(n1);
-    //     if (n2 != NULL)
-    //         q.push(n2);
-    // }
+    int a;
+    cin >> a;
+    TreeNode *root = new TreeNode(a);
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *presentRoot = q.front();
+        q.pop();
+        int x, y;
+        cin >> x >> y;
+        TreeNode *n1 = NULL;
+        TreeNode *n2 = NULL;
+        if (x != -1)
+            n1 = new TreeNode(x);
+        if (y != -1)
+            n2 = new TreeNode(y);
+        presentRoot->left = n1;
+        presentRoot->right = n2;
+        if (n1 != NULL)
+            q.push(n1);
+        if (n2 != NULL)
+            q.push(n2);
+    }
 
     Solution st;
-    vector<int> nums = {0, 1, 2, 3, 4, 5};
-    TreeNode *root2 = st.sortedArrayToBST(nums);
+    // vector<int> nums = {0, 1, 2, 3, 4, 5};
+    // TreeNode *root2 = st.sortedArrayToBST(nums);
+
+    string str1 = "";
+    string str2 = "";
+    string str3 = "";
+    string str4 = "";
+
+    inOrder(root, str1);
+    preOrder(root, str2);
+    postOrder(root, str3);
+    levelOrderTraversal(root, str4, 0);
+
+    cout << endl
+         << "In: " << str1 << endl
+         << endl;
+    cout << "Pre: " << str2 << endl
+         << endl;
+    cout << "Post: " << str3 << endl
+         << endl;
+    cout << "Level: " << str4 << endl
+         << endl;
+
+    cout << "Height of tree is " << maxDepth(root);
 
     // TreeNode *root2 = st.pruneTree(root);
-    printTree(root2, 0);
+    printTree(root, 0);
     // printTree(root2, 0);
 
     return 0;
