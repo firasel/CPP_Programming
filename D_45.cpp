5529b7f8cb5ff87d1e900ba8e8af1ab5482259a9
firasel <mdrasel601390@gmail.com>
2022-09-24 15:35:05 +0600

    Problem 6 solved

--- D_45.cpp
+++ D_45.cpp
@@ -1,157 +1,59 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <math.h>
 #include <string.h>
-#include <map>
-#include <vector>
 using namespace std;
 
-class TreeNode
+string colorMixing(string s)
 {
-public:
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int value)
+    string result = "";
+    for (int i = 0; i < s.length(); i++)
     {
-        val = value;
-        left = NULL;
-        right = NULL;
-    }
-};
-
-void removeMaxToTree(TreeNode *&root, int &result)
-{
-    if (root == NULL)
-        return;
-
-    if (root->right != NULL && root->right->right != NULL)
-    {
-        removeMaxToTree(root->right, result);
-    }
-    else if (root->right != NULL)
-    {
-        TreeNode *del = root->right;
-        result = del->val;
-        root->right = del->left;
-        delete del;
-    }
-    else if (root->right == NULL)
-    {
-        TreeNode *del = root;
-        result = del->val;
-        root = del->left;
-        delete del;
-    }
-}
-
-void insertNode(TreeNode *&root, int val)
-{
-    if (root == NULL)
-    {
-        root = new TreeNode(val);
-        return;
-    }
-
-    if (root->val > val)
-    {
-        if (root->left)
-            insertNode(root->left, val);
+        if (s[i] != s[i + 1])
+        {
+            if ((s[i] == 'R' && s[i + 1] == 'B') || (s[i] == 'B' && s[i + 1] == 'R'))
+            {
+                result += 'P';
+                i++;
+            }
+            else if ((s[i] == 'R' && s[i + 1] == 'G') || (s[i] == 'G' && s[i + 1] == 'R'))
+            {
+                result += 'Y';
+                i++;
+            }
+            else if ((s[i] == 'B' && s[i + 1] == 'G') || (s[i] == 'G' && s[i + 1] == 'B'))
+            {
+                result += 'C';
+                i++;
+            }
+            else if (s[i] != s[i + 1])
+            {
+                result.push_back(s[i]);
+            }
+        }
         else
-        {
-            root->left = new TreeNode(val);
-        }
+            i++;
     }
-    else
-    {
-        if (root->right)
-            insertNode(root->right, val);
-        else
-        {
-            root->right = new TreeNode(val);
-        }
-    }
-}
-
-string colorMixing(int n, string s)
-{
-    string result;
-    int i = 0, j = 1;
-    while (j < n)
-    {
-        if ((s[i] == 'R' && s[j] == 'B') || (s[i] == 'B' && s[j] == 'R'))
-        {
-            result += 'P';
-            i++;
-            j++;
-        }
-        else if ((s[i] == 'R' && s[j] == 'G') || (s[i] == 'G' && s[j] == 'R'))
-        {
-            result += 'Y';
-            i++;
-            j++;
-        }
-        else if ((s[i] == 'B' && s[j] == 'G') || (s[i] == 'G' && s[j] == 'B'))
-        {
-            result += 'C';
-            i++;
-            j++;
-        }
-        else if (s[i] != s[j])
-        {
-            result.push_back(s[i]);
-            result.push_back(s[j]);
-        }
-        else if (s[i] == s[j])
-        {
-            i++;
-            j++;
-        }
-        i++;
-        j++;
-    }
-    if (n % 2 != 0)
-        result += s[n - 1];
     return result;
 }
 
 int main()
 {
-    int n, e, l;
-    cin >> n >> e;
-    map<int, vector<int>> levelShop;
-    for (int i = 0; i < e; i++)
+    int t;
+    cin >> t;
+    while (t)
     {
-        int a, b;
-        cin >> a >> b;
-        levelShop[a].push_back(b);
-        levelShop[b].push_back(a);
-    }
-
-    cin >> l;
-    map<int, bool> visited;
-    queue<pair<int, int>> q;
-    q.push(make_pair(0, 0));
-    visited[0] = true;
-
-    while (!q.empty())
-    {
-        auto node = q.front();
-        q.pop();
-        if (node.first == l)
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string prev = s, final = s;
+        do
         {
-            cout << node.second << " ";
-        }
-        for (auto it : levelShop[node.second])
-        {
-            if (!visited[it])
-            {
-                visited[it] = true;
-                q.push(make_pair((node.first + 1), it));
-            }
-        }
+            prev = final;
+            final = colorMixing(prev);
+        } while (prev != final);
+        cout << final << endl;
+        t--;
     }
-
     return 0;
 }
