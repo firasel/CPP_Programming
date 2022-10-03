c7f117a225d03e8a0d66b3c1185767e83f7fd9ad
firasel <mdrasel601390@gmail.com>
2022-10-03 18:35:32 +0600

    Binary Search Tree to Greater Sum Tree solved

--- D_47.cpp
+++ D_47.cpp
@@ -18,65 +18,23 @@ public:
 class Solution
 {
 public:
-  TreeNode *inOrderSuccessor(TreeNode *root)
-  {
-    TreeNode *curr = root;
-    while (curr->left != NULL)
-    {
-      curr = curr->left;
-    }
-    return curr;
-  }
-
-  TreeNode *deleteNode(TreeNode *root, int key)
+  TreeNode *bstToGstHelper(TreeNode *root, int &val)
   {
     if (root == NULL)
       return root;
-    if (root->val == key)
-    {
-      if (!root->left && !root->right)
-      {
-        delete root;
-        return NULL;
-      }
-      else if (root->left == NULL)
-      {
-        TreeNode *temp = root->right;
-        delete root;
-        return temp;
-      }
-      else if (root->right == NULL)
-      {
-        TreeNode *temp = root->left;
-        delete root;
-        return temp;
-      }
-      else
-      {
-        TreeNode *temp = inOrderSuccessor(root->right);
-        root->val = temp->val;
-        root->right = deleteNode(root->right, temp->val);
-      }
-    }
+    bstToGstHelper(root->right, val);
+    root->val = root->val + val;
+    val = root->val;
+    bstToGstHelper(root->left, val);
     return root;
   }
 
-  TreeNode *trimBST(TreeNode *root, int low, int high)
+  TreeNode *bstToGst(TreeNode *root)
   {
     if (root == NULL)
       return root;
-    if (root->val >= low && root->val <= high)
-    {
-      root->left = trimBST(root->left, low, high);
-      root->right = trimBST(root->right, low, high);
-    }
-    else
-    {
-      root = deleteNode(root, root->val);
-      trimBST(root, low, high);
-    }
-
-    return root;
+    int sum = 0;
+    return bstToGstHelper(root, sum);
   }
 };
 
@@ -92,21 +50,29 @@ void preOrder(TreeNode *root)
 int main()
 {
   Solution st;
-  TreeNode *mainRoot = new TreeNode(2);
+  TreeNode *mainRoot = new TreeNode(4);
   TreeNode *root1 = new TreeNode(1);
-  TreeNode *root2 = new TreeNode(3);
-  // TreeNode *root3 = new TreeNode(1);
-  // TreeNode *root4 = new TreeNode(4);
+  TreeNode *root2 = new TreeNode(6);
+  TreeNode *root3 = new TreeNode(0);
+  TreeNode *root4 = new TreeNode(2);
+  TreeNode *root5 = new TreeNode(5);
+  TreeNode *root6 = new TreeNode(7);
+  TreeNode *root7 = new TreeNode(3);
+  TreeNode *root8 = new TreeNode(8);
 
   mainRoot->left = root1;
   mainRoot->right = root2;
-  // root1->right = root2;
-  // root2->left = root3;
-  // mainRoot->right = root4;
+  root1->left = root3;
+  root1->right = root4;
+  root4->right = root7;
+  root2->left = root5;
+  root2->right = root6;
+  root6->right = root8;
 
   preOrder(mainRoot);
   cout << endl;
-  TreeNode *root = st.trimBST(mainRoot, 3, 4);
+  TreeNode *root = st.bstToGst(mainRoot);
+  cout << endl;
   preOrder(root);
   return 0;
 }
