c1492470ae563590920f639cb3db8f9d0c522c4b
firasel <mdrasel601390@gmail.com>
2022-10-05 18:38:49 +0600

    Delete Node in a Linked List

--- D_48.cpp
+++ D_48.cpp
@@ -16,37 +16,12 @@ public:
 class Solution
 {
 public:
-  ListNode *mergeInBetween(ListNode *list1, int a, int b, ListNode *list2)
+  void deleteNode(ListNode *node)
   {
-    ListNode *prev = NULL;
-    ListNode *curr = list1;
-    ListNode *start;
-    ListNode *end;
-    int count = 0;
-
-    while (curr != NULL)
-    {
-      if (count == a)
-        start = prev;
-      if (count == b)
-      {
-        end = curr->next;
-        break;
-      }
-      prev = curr;
-      curr = curr->next;
-      count++;
-    }
-
-    if (start)
-      start->next = list2;
-    else
-      list1 = list2;
-    while (list2->next != NULL)
-      list2 = list2->next;
-    list2->next = end;
-
-    return list1;
+    ListNode *del = node->next;
+    node->val = node->next->val;
+    node->next = del->next;
+    delete del;
   }
 };
 
@@ -60,27 +35,17 @@ void printList(ListNode *head)
 
 int main()
 {
-  ListNode *head = new ListNode(0);
-  ListNode *node1 = new ListNode(1);
-  ListNode *node2 = new ListNode(2);
-  ListNode *node3 = new ListNode(3);
-  ListNode *node4 = new ListNode(4);
-  ListNode *node5 = new ListNode(5);
+  ListNode *head = new ListNode(4);
+  ListNode *node1 = new ListNode(5);
+  ListNode *node2 = new ListNode(1);
+  ListNode *node3 = new ListNode(9);
   head->next = node1;
   node1->next = node2;
   node2->next = node3;
-  node3->next = node4;
-  node4->next = node5;
-
-  ListNode *head2 = new ListNode(10);
-  ListNode *node2_1 = new ListNode(15);
-  ListNode *node2_2 = new ListNode(20);
-  head2->next = node2_1;
-  node2_1->next = node2_2;
 
   Solution st;
-  ListNode *result = st.mergeInBetween(head, 3, 4, head2);
-  printList(result);
+  st.deleteNode(node1);
+  printList(head);
   cout << endl;
   return 0;
 };
