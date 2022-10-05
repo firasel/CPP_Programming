04e4c89acdfb6c8796f770a300df08b586e2d9f7
firasel <mdrasel601390@gmail.com>
2022-10-05 20:55:14 +0600

    Flatten Binary Tree to Linked List

--- D_48.cpp
+++ D_48.cpp
@@ -1,63 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class ListNode
+class TreeNode
 {
 public:
   int val;
-  ListNode *next;
-  ListNode(int val)
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int val)
   {
     this->val = val;
-    next = NULL;
+    left = NULL;
+    right = NULL;
   }
 };
 
 class Solution
 {
 public:
-  ListNode *swapNodes(ListNode *head, int k)
+  void preOrder(TreeNode *root, vector<TreeNode *> &lists)
   {
-    vector<ListNode *> lists;
-    while (head != NULL)
-    {
-      lists.push_back(head);
-      head = head->next;
-    }
+    if (root == NULL)
+      return;
+    lists.push_back(root);
+    preOrder(root->left, lists);
+    preOrder(root->right, lists);
+  }
+  void flatten(TreeNode *root)
+  {
+    if (root == NULL)
+      return;
+    vector<TreeNode *> lists;
+    preOrder(root, lists);
     int size = lists.size();
-    swap(lists[k - 1], lists[size - k]);
-    lists[size - 1]->next = NULL;
     for (int i = 0; i < size - 1; i++)
     {
-      lists[i]->next = lists[i + 1];
+      lists[i]->left = NULL;
+      lists[i]->right = lists[i + 1];
     }
-    return lists[0];
   }
 };
 
-void printList(ListNode *head)
+void printList(TreeNode *head)
 {
   if (head == NULL)
     return;
   cout << head->val << " ";
-  printList(head->next);
+  printList(head->right);
 }
 
 int main()
 {
-  ListNode *head = new ListNode(1);
-  ListNode *node1 = new ListNode(2);
-  ListNode *node2 = new ListNode(3);
-  ListNode *node3 = new ListNode(4);
-  ListNode *node4 = new ListNode(5);
-  head->next = node1;
-  node1->next = node2;
-  node2->next = node3;
-  node3->next = node4;
+  TreeNode *root = new TreeNode(1);
+  TreeNode *node1 = new TreeNode(2);
+  TreeNode *node2 = new TreeNode(3);
+  TreeNode *node3 = new TreeNode(4);
+  TreeNode *node4 = new TreeNode(5);
+  TreeNode *node5 = new TreeNode(6);
+  root->left = node1;
+  node1->left = node2;
+  node1->right = node3;
+  root->right = node4;
+  node4->right = node5;
 
   Solution st;
-  st.swapNodes(head, 2);
-  printList(head);
+  st.flatten(root);
+  printList(root);
   cout << endl;
   return 0;
 };
