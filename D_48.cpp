a6044cf83760e4686232888ff505f009daef3faf
firasel <mdrasel601390@gmail.com>
2022-10-05 12:39:24 +0600

    Linked List Components

--- D_48.cpp
+++ D_48.cpp
@@ -1,28 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class ListNode
+{
+public:
+  int val;
+  ListNode *next;
+  ListNode(int val)
+  {
+    this->val = val;
+    next = NULL;
+  }
+};
+
 class Solution
 {
 public:
-  vector<int> decompressRLElist(vector<int> &nums)
+  int numComponents(ListNode *head, vector<int> &nums)
   {
-    vector<int> result;
-    for (int i = 0; i < nums.size(); i += 2)
+    int count = 0;
+    bool prev = false;
+    while (head != NULL)
     {
-      for (int j = 0; j < nums[i]; j++)
-        result.push_back(nums[i + 1]);
+      bool found = false;
+      for (auto i : nums)
+      {
+        if (i == head->val)
+        {
+          found = true;
+          prev = true;
+          break;
+        }
+      }
+      if (prev && !found)
+      {
+        count++;
+        prev = false;
+      }
+      head = head->next;
     }
-    return result;
+    if (prev)
+      count++;
+    return count;
   }
 };
 
 int main()
 {
+  ListNode *head = new ListNode(0);
+  ListNode *node1 = new ListNode(1);
+  ListNode *node2 = new ListNode(2);
+  ListNode *node3 = new ListNode(3);
+  head->next = node1;
+  node1->next = node2;
+  node2->next = node3;
+
   Solution st;
-  vector<int> nums = {1, 1, 2, 3, 3, 4};
-  vector<int> result = st.decompressRLElist(nums);
-  for (auto i : result)
-    cout << i << " ";
-  cout << endl;
+  vector<int> nums = {0, 1, 3};
+  int result = st.numComponents(head, nums);
+  cout << result << endl;
   return 0;
-}
+};
