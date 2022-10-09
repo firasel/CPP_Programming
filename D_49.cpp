753a11f16bff12680cc81bf5b604975965a9c5bd
firasel <mdrasel601390@gmail.com>
2022-10-09 11:10:18 +0600

    The Employee That Worked on the Longest Task

--- D_49.cpp
+++ D_49.cpp
@@ -5,46 +5,33 @@
 #include <string.h>
 using namespace std;
 
+class Solution
+{
+public:
+  int hardestWorker(int n, vector<vector<int>> &logs)
+  {
+    int id;
+    int max = 0, prev = 0, size = logs.size();
+    for (int i = 0; i < size; i++)
+    {
+      if (max < (logs[i][1] - prev))
+      {
+        max = logs[i][1] - prev;
+        id = logs[i][0];
+      }
+      else if (max == (logs[i][1] - prev))
+        id = min(id, logs[i][0]);
+      prev = logs[i][1];
+    }
+    return id;
+  }
+};
+
 int main()
 {
-  int n;
-  string s = "";
-  stack<string> history;
-  cin >> n;
-  while (n)
-  {
-    int op;
-    cin >> op;
-    if (op == 1)
-    {
-      history.push(s);
-      string temp;
-      cin >> temp;
-      s += temp;
-    }
-    else if (op == 2)
-    {
-      int len;
-      cin >> len;
-      history.push(s);
-      s = s.substr(0, s.length() - len);
-    }
-    else if (op == 3)
-    {
-      int pos;
-      cin >> pos;
-      cout << s[pos - 1] << endl;
-    }
-    else if (op == 4)
-    {
-      if (!history.empty())
-      {
-        s = history.top();
-        history.pop();
-      }
-    }
-    n--;
-  }
+  Solution st;
+  vector<vector<int>> logs = {{0, 3}, {2, 5}, {0, 9}, {1, 15}};
+  cout << st.hardestWorker(10, logs) << endl;
 
   return 0;
 }
