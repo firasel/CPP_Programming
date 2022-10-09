825b4a9caffe4688ff0590e0d1196e3672677b16
firasel <mdrasel601390@gmail.com>
2022-10-09 21:26:40 +0600

    Peak Index in a Mountain Array

--- D_49.cpp
+++ D_49.cpp
@@ -8,28 +8,19 @@ using namespace std;
 class Solution
 {
 public:
-  int searchInsert(vector<int> &nums, int target)
+  int peakIndexInMountainArray(vector<int> &arr)
   {
-    int start = 0, end = nums.size() - 1;
-    if (nums.size() == 0)
-      return start;
+    int start = 0, end = arr.size() - 1;
     while (start <= end)
     {
       int mid = (start + end) / 2;
-      if (nums[mid] > target)
-      {
-        end = mid - 1;
-      }
-      else if (nums[mid] < target)
-      {
-        start = mid + 1;
-      }
-      else if (nums[mid] == target)
-      {
+      if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
         return mid;
-      }
+      else if (arr[mid] < arr[mid - 1])
+        end = mid - 1;
+      else
+        start = mid + 1;
     }
-
     return start;
   }
 };
@@ -37,8 +28,8 @@ public:
 int main()
 {
   Solution st;
-  vector<int> nums = {1, 3, 5, 6};
-  cout << st.searchInsert(nums, 7) << endl;
+  vector<int> nums = {0, 1, 2, 3, 3, 3, 4, 2, 1, 0, 0};
+  cout << st.peakIndexInMountainArray(nums) << endl;
 
   return 0;
 }
