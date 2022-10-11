74ebc19b100120c4edac8cfd2dd8a1d9931a8489
firasel <mdrasel601390@gmail.com>
2022-10-11 18:52:55 +0600

    Sqrt(x) problem solved

--- D_49.cpp
+++ D_49.cpp
@@ -8,33 +8,27 @@ using namespace std;
 class Solution
 {
 public:
-  int findTheDistanceValue(vector<int> &arr1, vector<int> &arr2, int d)
+  int mySqrt(int x)
   {
-    int count = 0;
-    for (auto num : arr1)
+    if (x < 2)
+      return x;
+    int start = 0, end = x, mid;
+    while (start < end)
     {
-      bool chk = true;
-      for (auto num2 : arr2)
-      {
-        if (abs(num - num2) <= d)
-        {
-          chk = false;
-          break;
-        }
-      }
-      if (chk)
-        count++;
+      int mid = (start + end) / 2;
+      if ((x / mid) >= mid)
+        start = mid + 1;
+      else
+        end = mid;
     }
-    return count;
+    return end - 1;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums = {4, 5, 8};
-  vector<int> nums2 = {10, 9, 1, 8};
-  cout << st.findTheDistanceValue(nums, nums2, 2) << endl;
+  cout << st.mySqrt(16) << endl;
 
   return 0;
 }
