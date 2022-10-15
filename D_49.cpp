eb296ea608a5409ff69a87daf6328317b08b08ca
firasel <mdrasel601390@gmail.com>
2022-10-15 17:39:52 +0600

    Search a 2D Matrix

--- D_49.cpp
+++ D_49.cpp
@@ -8,25 +8,37 @@ using namespace std;
 class Solution
 {
 public:
-  int countNegatives(vector<vector<int>> &grid)
+  bool searchMatrix(vector<vector<int>> &matrix, int target)
   {
-    int count = 0;
-    for (auto nums : grid)
+    for (auto nums : matrix)
     {
       int size = nums.size() - 1;
-      for (int i = size; i >= 0; i--)
-        if (nums[i] < 0)
-          count++;
+      if (nums[0] > target)
+        return false;
+      if (nums[size] >= target)
+      {
+        int start = 0, end = size, mid;
+        while (start <= end)
+        {
+          mid = (start + end) / 2;
+          if (nums[mid] == target)
+            return true;
+          else if (nums[mid] < target)
+            start = mid + 1;
+          else
+            end = mid - 1;
+        }
+      }
     }
-    return count;
+    return false;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<vector<int>> grid = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
-  cout << st.countNegatives(grid) << endl;
+  vector<vector<int>> grid = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+  cout << st.searchMatrix(grid, 3) << endl;
 
   return 0;
 }
