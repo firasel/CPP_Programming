1ea2510d6aa794dabdb15be68411d407c8abffbe
firasel <mdrasel601390@gmail.com>
2022-10-16 08:52:19 +0600

    Largest Positive Integer That Exists With Its Negative

--- D_49.cpp
+++ D_49.cpp
@@ -8,60 +8,38 @@ using namespace std;
 class Solution
 {
 public:
-  int countTime(string time)
+  int findMaxK(vector<int> &nums)
   {
-    int h = 1, m = 1;
-    if (time[0] == '?' && time[1] == '?')
-      h = 24;
-    else if (time[0] == '?')
+    sort(nums.begin(), nums.end());
+    int size = nums.size() - 1;
+    for (int i = size; i >= 0; i--)
     {
-      int num = time[1] - '0';
-      int count = 0;
-      while (true)
-      {
-        int res = stoi(to_string(count) + to_string(num));
-        if (res > 23)
-          break;
-        count++;
-      }
-      h = count;
-    }
-    else if (time[1] == '?')
-    {
-      if (time[0] == '2')
-        h = 4;
+      if (nums[i] < 0)
+        break;
       else
-        h = 10;
-    }
-
-    if (time[3] == '?' && time[4] == '?')
-      m = 60;
-    else if (time[3] == '?')
-    {
-      int num = time[4] - '0';
-      int count = 0;
-      while (true)
       {
-        int res = stoi(to_string(count) + to_string(num));
-        if (res > 59)
-          break;
-        count++;
+        int start = 0, end = size, target = nums[i] * -1;
+        while (start <= end)
+        {
+          int mid = (start + end) / 2;
+          if (nums[mid] == target)
+            return target * -1;
+          else if (nums[mid] < target)
+            start = mid + 1;
+          else
+            end = mid - 1;
+        }
       }
-      m = count;
     }
-    else if (time[4] == '?')
-      m = 10;
-
-    return h * m;
+    return -1;
   }
 };
 
 int main()
 {
   Solution st;
-  cout << st.countTime("?5:00") << endl;
-  cout << st.countTime("0?:0?") << endl;
-  cout << st.countTime("??:??") << endl;
+  vector<int> nums = {-1, 10, 6, 7, -7, 1};
+  cout << st.findMaxK(nums) << endl;
 
   return 0;
 }
