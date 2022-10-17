096a6b9c9d28b88a2539146425c58cd785583a41
firasel <mdrasel601390@gmail.com>
2022-10-17 19:56:41 +0600

    Sum of Square Numbers

--- D_49.cpp
+++ D_49.cpp
@@ -8,60 +8,27 @@ using namespace std;
 class Solution
 {
 public:
-  vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+  bool judgeSquareSum(int c)
   {
-    sort(nums1.begin(), nums1.end());
-    sort(nums2.begin(), nums2.end());
-    vector<int> result;
-    if (nums1.size() < nums2.size())
+    long long int left = 0, right = sqrt(c);
+    while (left <= right)
     {
-      for (auto num : nums1)
-      {
-        int size = nums2.size();
-        for (int i = 0; i < size; i++)
-        {
-          if (num < nums2[i])
-            break;
-          if (num == nums2[i])
-          {
-            result.push_back(num);
-            nums2[i] = -1;
-            break;
-          }
-        }
-      }
+      long long int total = left * left + right * right;
+      if (total < c)
+        left++;
+      else if (total > c)
+        right--;
+      else
+        return true;
     }
-    else
-    {
-      for (auto num : nums2)
-      {
-        int size = nums1.size();
-        for (int i = 0; i < size; i++)
-        {
-          if (num < nums1[i])
-            break;
-          if (num == nums1[i])
-          {
-            result.push_back(num);
-            nums1[i] = -1;
-            break;
-          }
-        }
-      }
-    }
-    return result;
+    return false;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums1 = {1, 2, 2, 1};
-  vector<int> nums2 = {2, 2};
-  vector<int> res = st.intersect(nums1, nums2);
-  for (auto num : res)
-    cout << num << " ";
-  cout << endl;
+  cout << st.judgeSquareSum(5) << endl;
 
   return 0;
 }
