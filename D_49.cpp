4809e1f50f57205d18e2a7890e1d3e311fdf575f
firasel <mdrasel601390@gmail.com>
2022-10-17 19:36:51 +0600

    Intersection of Two Arrays II

--- D_49.cpp
+++ D_49.cpp
@@ -8,45 +8,60 @@ using namespace std;
 class Solution
 {
 public:
-  int countDistinctIntegers(vector<int> &nums)
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
   {
-    map<int, int> numCount;
-    for (auto num : nums)
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+    vector<int> result;
+    if (nums1.size() < nums2.size())
     {
-      int revNum = 0, prevNum = num;
-      while (num > 0)
+      for (auto num : nums1)
       {
-        revNum = (revNum * 10) + (num % 10);
-        num /= 10;
+        int size = nums2.size();
+        for (int i = 0; i < size; i++)
+        {
+          if (num < nums2[i])
+            break;
+          if (num == nums2[i])
+          {
+            result.push_back(num);
+            nums2[i] = -1;
+            break;
+          }
+        }
       }
-      numCount[revNum] = 1;
-      numCount[prevNum] = 1;
     }
-    return numCount.size();
-  }
-  bool sumOfNumberAndReverse(int num)
-  {
-    int i = num / 2;
-    for (i; i <= num; i++)
+    else
     {
-      int revNum = 0, currNum = i;
-      while (currNum > 0)
+      for (auto num : nums2)
       {
-        revNum = (revNum * 10) + (currNum % 10);
-        currNum /= 10;
+        int size = nums1.size();
+        for (int i = 0; i < size; i++)
+        {
+          if (num < nums1[i])
+            break;
+          if (num == nums1[i])
+          {
+            result.push_back(num);
+            nums1[i] = -1;
+            break;
+          }
+        }
       }
-      if (revNum + i == num)
-        return true;
     }
-    return false;
+    return result;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums = {1, 13, 10, 12, 31};
-  cout << st.sumOfNumberAndReverse(181) << endl;
+  vector<int> nums1 = {1, 2, 2, 1};
+  vector<int> nums2 = {2, 2};
+  vector<int> res = st.intersect(nums1, nums2);
+  for (auto num : res)
+    cout << num << " ";
+  cout << endl;
 
   return 0;
 }
