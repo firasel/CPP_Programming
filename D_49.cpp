b8213c91a446c2ff5ac384a2b42c8a5155dae1bd
firasel <mdrasel601390@gmail.com>
2022-10-18 23:40:11 +0600

    Tourist Translations

--- D_49.cpp
+++ D_49.cpp
@@ -1,34 +1,35 @@
-#include <bits/stdc++.h>
 #include <iostream>
-#include <stack>
-#include <string>
-#include <string.h>
+#include <vector>
+#include <unordered_map>
 using namespace std;
-
-class Solution
-{
-public:
-  int search(vector<int> &nums, int target)
-  {
-    int start = 0, end = nums.size() - 1;
-    while (start <= end)
-    {
-      if (nums[start] == target)
-        return start;
-      else if (nums[end] == target)
-        return end;
-      start++;
-      end--;
-    }
-    return -1;
-  }
-};
 
 int main()
 {
-  Solution st;
-  vector<int> nums1 = {4, 5, 6, 7, 0, 1, 2};
-  cout << st.search(nums1, 3) << endl;
-
+  int t;
+  string m;
+  unordered_map<char, char> chMap;
+  cin >> t >> m;
+  char temp = 'a';
+  for (auto ch : m)
+  {
+    chMap[temp] = ch;
+    chMap[temp - 32] = ch - 32;
+    temp++;
+  }
+  while (t--)
+  {
+    string str, res = "";
+    cin >> str;
+    for (auto ch : str)
+    {
+      if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        res += chMap[ch];
+      else if (ch == '_')
+        res += " ";
+      else
+        res += ch;
+    }
+    cout << res << endl;
+  }
   return 0;
 }
