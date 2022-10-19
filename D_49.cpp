ceb9eac6537f510f30ea4b64def84d77713eb690
firasel <mdrasel601390@gmail.com>
2022-10-19 07:00:15 +0600

    Top K Frequent Words

--- D_49.cpp
+++ D_49.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <map>
+#include <set>
+#include <algorithm>
 using namespace std;
+
+class Solution
+{
+public:
+  vector<string> topKFrequent(vector<string> &words, int k)
+  {
+    vector<string> res;
+    sort(words.begin(), words.end());
+    map<string, int> freqMap;
+    for (auto str : words)
+      freqMap[str]++;
+    multimap<int, string, greater<int>> freqSrtMap;
+
+    for (auto el : freqMap)
+      freqSrtMap.insert({el.second, el.first});
+    int i = 0;
+    for (auto el : freqSrtMap)
+    {
+      res.push_back(el.second);
+      i++;
+      if (i >= k)
+        break;
+    }
+    return res;
+  }
+};
 
 int main()
 {
-  int t;
-  string m;
-  unordered_map<char, char> chMap;
-  cin >> t >> m;
-  char temp = 'a';
-  for (auto ch : m)
-  {
-    chMap[temp] = ch;
-    chMap[temp - 32] = ch - 32;
-    temp++;
-  }
-  while (t--)
-  {
-    string str, res = "";
-    cin >> str;
-    for (auto ch : str)
-    {
-      if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-        res += chMap[ch];
-      else if (ch == '_')
-        res += " ";
-      else
-        res += ch;
-    }
-    cout << res << endl;
-  }
+  Solution st;
+  vector<string> words = {"i", "love", "leetcode", "i", "love", "coding"};
+  vector<string> res = st.topKFrequent(words, 2);
+  for (auto str : res)
+    cout << str << " ";
+  cout << endl;
   return 0;
 }
