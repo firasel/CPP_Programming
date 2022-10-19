c0b0a98bca74241e919a2db04403384a7e07473d
firasel <mdrasel601390@gmail.com>
2022-10-19 12:28:40 +0600

    The Playboy Chimp

--- D_49.cpp
+++ D_49.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int f91(int num)
-{
-  int res = num;
-  if (num < 101)
-    res = f91(f91(num + 11));
-  else
-    res = num - 10;
-  return res;
-}
 
 int main()
 {
-  int num;
-  while (cin >> num && num)
-    cout << "f91(" << num << ") = " << f91(num) << endl;
+  int n, q, pos = -1;
+  cin >> n;
+  vector<int> nums;
+  for (int i = 0; i < n; i++)
+  {
+    int num;
+    cin >> num;
+    if (nums.size() == 0 || nums[pos] != num)
+    {
+      nums.push_back(num);
+      pos++;
+    }
+  }
+  cin >> q;
+  while (q--)
+  {
+    int num;
+    cin >> num;
+    int sl = 0, tl = INT32_MAX;
+    for (auto el : nums)
+    {
+      if (el < num && el > sl)
+        sl = el;
+      if (el > num && el < tl)
+        tl = el;
+    }
+    if (sl == 0)
+      cout << "X ";
+    else
+      cout << sl << " ";
+    if (tl == INT32_MAX)
+      cout << "X" << endl;
+    else
+      cout << tl << endl;
+  }
   return 0;
 }
