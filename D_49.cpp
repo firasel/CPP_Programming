95ca146fd05264d1375bd5817642099852cd5156
firasel <mdrasel601390@gmail.com>
2022-10-20 12:58:05 +0600

    Sinus Dances

--- D_49.cpp
+++ D_49.cpp
@@ -1,38 +1,36 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
+
+void A(int n, int pos)
+{
+  cout << "sin(" << pos;
+  if (pos < n)
+  {
+    if (pos & 1)
+      cout << "-";
+    else
+      cout << "+";
+    A(n, pos + 1);
+  }
+  cout << ")";
+}
+
+void S(int n, int pos)
+{
+  if (pos < n)
+  {
+    cout << "(";
+    S(n, pos + 1);
+    cout << ")";
+  }
+  A(n + 1 - pos, 1);
+  cout << "+" << pos;
+}
 
 int main()
 {
-  int t, n, c;
-  cin >> t;
-  while (t--)
-  {
-    cin >> n >> c;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++)
-      cin >> nums[i];
-    sort(nums.begin(), nums.end());
-    int start = 0, end = nums[n - 1], mid, res = 0;
-    while (start <= end)
-    {
-      mid = (start + end) / 2;
-      int cCount = 1, prev = 0;
-      for (int i = 1; i < n && cCount < c; i++)
-      {
-        if (nums[i] - nums[prev] >= mid)
-          prev = i, cCount++;
-      }
-      if (cCount >= c)
-      {
-        res = mid;
-        start = mid + 1;
-      }
-      else
-        end = mid - 1;
-    }
-    cout << res << endl;
-  }
+  int n;
+  cin >> n;
+  S(n, 1);
   return 0;
 }
