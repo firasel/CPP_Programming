449f9d0136085a06c6db2133513b65a17631f031
firasel <mdrasel601390@gmail.com>
2022-10-23 21:32:13 +0600

    Modify the Pascal's Triangle

--- D_49.cpp
+++ D_49.cpp
@@ -7,11 +7,7 @@ public:
   vector<vector<int>> generate(int numRows)
   {
     vector<vector<int>> res;
-    if (numRows >= 1)
-      res.push_back({1});
-    if (numRows >= 2)
-      res.push_back({1, 1});
-    for (int i = 2; i < numRows; i++)
+    for (int i = 0; i < numRows; i++)
     {
       vector<int> temp;
       for (int j = 0; j <= i; j++)
