49d93467ccb88fdf3079a790b022b643d7c82863
firasel <mdrasel601390@gmail.com>
2022-10-25 23:18:25 +0600

    First Unique Character in a String

--- D_49.cpp
+++ D_49.cpp
@@ -4,89 +4,27 @@ using namespace std;
 class Solution
 {
 public:
-  bool isValidSudoku(vector<vector<char>> &board)
+  int firstUniqChar(string s)
   {
-    map<char, int> freq;
-    // Row valid check
-    for (int i = 0; i < 9; i++)
+    unordered_map<char, int> charMap;
+    for (int i = 0; i < s.length(); i++)
     {
-      for (auto ch : board[i])
+      if (charMap[s[i]] == 0)
       {
-        if (ch != '.')
-        {
-          freq[ch]++;
-          if (freq[ch] > 1)
-            return false;
-        }
+        size_t found = s.find(s[i], i + 1);
+        if (found == string::npos)
+          return i;
+        charMap[s[i]]++;
       }
-      freq.clear();
     }
-    // Col valid check
-    for (int i = 0; i < 9; i++)
-    {
-      for (int j = 0; j < 9; j++)
-      {
-        if (board[j][i] != '.')
-        {
-          freq[board[j][i]]++;
-          if (freq[board[j][i]] > 1)
-            return false;
-        }
-      }
-      freq.clear();
-    }
-    // Cube valid check
-    int jLim = 3, kLim = 3, count = 0;
-    for (int i = 0; i < 9; i++)
-    {
-      for (int j = jLim - 3; j < jLim; j++)
-      {
-        for (int k = kLim - 3; k < kLim; k++)
-        {
-          cout << board[j][k] << " ";
-          if (board[j][k] != '.')
-          {
-            freq[board[j][k]]++;
-            if (freq[board[j][k]] > 1)
-              return false;
-          }
-        }
-        cout << endl;
-      }
-      cout << endl
-           << endl;
-      count++;
-      if (count == 3)
-      {
-        count = 0;
-        jLim += 3;
-      }
-      if (kLim >= 9)
-      {
-        kLim = 0;
-      }
-      kLim += 3;
-      freq.clear();
-    }
-    return true;
+    return -1;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<vector<char>> board =
-      {
-          {'.', '.', '4', '.', '.', '.', '6', '3', '.'},
-          {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-          {'5', '.', '.', '.', '.', '.', '.', '9', '.'},
-          {'.', '.', '.', '5', '6', '.', '.', '.', '.'},
-          {'4', '.', '3', '.', '.', '.', '.', '.', '1'},
-          {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
-          {'.', '.', '.', '5', '.', '.', '.', '.', '.'},
-          {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-          {'.', '.', '.', '.', '.', '.', '.', '.', '.'}};
-  bool res = st.isValidSudoku(board);
+  int res = st.firstUniqChar("aab");
   cout << res << endl;
 
   return 0;
