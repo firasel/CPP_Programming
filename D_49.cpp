ef113e49462cf12c21da0b0740bebe7ec33d2949
firasel <mdrasel601390@gmail.com>
2022-10-26 21:01:32 +0600

    Linked List Cycle

--- D_49.cpp
+++ D_49.cpp
@@ -16,53 +16,18 @@ public:
 class Solution
 {
 public:
-  ListNode *head = NULL;
-
-  void insertAtTail(int val)
+  bool hasCycle(ListNode *head)
   {
-    ListNode *newNode = new ListNode(val);
-    if (head == NULL)
-      head = newNode;
-    else
+    ListNode *s = head;
+    ListNode *f = head;
+    while (f != NULL && f->next != NULL)
     {
-      ListNode *temp = head;
-      while (temp->next != NULL)
-      {
-        temp = temp->next;
-      }
-      temp->next = newNode;
+      if (s->next == f->next->next)
+        return true;
+      s = s->next;
+      f = f->next->next;
     }
-  }
-
-  void remainderList(ListNode *node)
-  {
-    while (node != NULL)
-    {
-      insertAtTail(node->val);
-      node = node->next;
-    }
-  }
-
-  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
-  {
-    ListNode *a = list1;
-    ListNode *b = list2;
-    while (a != NULL && b != NULL)
-    {
-      if (a->val <= b->val)
-      {
-        insertAtTail(a->val);
-        a = a->next;
-      }
-      else
-      {
-        insertAtTail(b->val);
-        b = b->next;
-      }
-    }
-    remainderList(a);
-    remainderList(b);
-    return head;
+    return false;
   }
 };
 
@@ -76,22 +41,19 @@ void printList(ListNode *head)
 
 int main()
 {
-  ListNode *root1 = new ListNode(1);
-  ListNode *node1_1 = new ListNode(2);
-  ListNode *node1_2 = new ListNode(4);
+  ListNode *root = new ListNode(3);
+  ListNode *node1 = new ListNode(2);
+  ListNode *node2 = new ListNode(0);
+  ListNode *node3 = new ListNode(4);
 
-  ListNode *root2 = new ListNode(2);
-  ListNode *node2_1 = new ListNode(4);
-  ListNode *node2_2 = new ListNode(3);
-  root1->next = node1_1;
-  node1_1->next = node1_2;
-
-  root2->next = node2_1;
-  node2_1->next = node2_2;
+  root->next = node1;
+  node1->next = node2;
+  node2->next = node3;
+  node3->next = node1;
 
   Solution st;
-  ListNode *res = st.mergeTwoLists(root1, root2);
-  printList(res);
+  bool res = st.hasCycle(root);
+  cout << res << endl;
 
   return 0;
 }
