8526607ea30346983aca45e01ce7cc37fd8e2d4c
firasel <mdrasel601390@gmail.com>
2022-10-28 16:45:21 +0600

    Reverse Nodes in k-Group

--- D_49.cpp
+++ D_49.cpp
@@ -13,47 +13,6 @@ public:
   }
 };
 
-class Solution
-{
-public:
-  void traverseList(ListNode *head, vector<int> &nums)
-  {
-    if (head == NULL)
-      return;
-    nums.push_back(head->val);
-    traverseList(head->next, nums);
-  }
-
-  ListNode *makeLinkedList(vector<int> nums)
-  {
-    ListNode *head = NULL;
-    ListNode *curr = NULL;
-    for (auto num : nums)
-    {
-      if (head == NULL)
-      {
-        head = new ListNode(num);
-        curr = head;
-      }
-      else
-      {
-        curr->next = new ListNode(num);
-        curr = curr->next;
-      }
-    }
-    return head;
-  }
-
-  ListNode *mergeKLists(vector<ListNode *> &lists)
-  {
-    vector<int> nums;
-    for (auto head : lists)
-      traverseList(head, nums);
-    sort(nums.begin(), nums.end());
-    return makeLinkedList(nums);
-  }
-};
-
 void printList(ListNode *head)
 {
   if (head == NULL)
@@ -62,31 +21,51 @@ void printList(ListNode *head)
   printList(head->next);
 }
 
+class Solution
+{
+public:
+  ListNode *reverseKGroup(ListNode *head, int k)
+  {
+    if (head == NULL)
+      return head;
+    int count = 0;
+    ListNode *prevHead = head;
+    ListNode *temp = head;
+    stack<int> nums;
+    while (temp != NULL)
+    {
+      nums.push(temp->val);
+      if (++count == k)
+      {
+        count = 0;
+        while (!nums.empty() && prevHead != NULL)
+        {
+          prevHead->val = nums.top();
+          nums.pop();
+          prevHead = prevHead->next;
+        }
+      }
+      temp = temp->next;
+    }
+    return head;
+  }
+};
+
 int main()
 {
   ListNode *root = new ListNode(1);
   ListNode *node1 = new ListNode(2);
-  ListNode *node2 = new ListNode(6);
-  ListNode *node3 = new ListNode(3);
-  ListNode *node4 = new ListNode(4);
+  ListNode *node2 = new ListNode(3);
+  ListNode *node3 = new ListNode(4);
+  ListNode *node4 = new ListNode(5);
 
   root->next = node1;
   node1->next = node2;
   node2->next = node3;
   node3->next = node4;
 
-  ListNode *root2 = new ListNode(1);
-  ListNode *node2_1 = new ListNode(2);
-  ListNode *node2_2 = new ListNode(6);
-  ListNode *node2_3 = new ListNode(3);
-
-  root2->next = node2_1;
-  node2_1->next = node2_2;
-  node2_2->next = node2_3;
-
   Solution st;
-  vector<ListNode *> lists = {root, root2};
-  ListNode *res = st.mergeKLists(lists);
+  ListNode *res = st.reverseKGroup(root, 3);
   printList(res);
 
   return 0;
