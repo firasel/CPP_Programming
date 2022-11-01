f83bbc62a5c3dc919e90ea19244b10a9a8b236e0
firasel <mdrasel601390@gmail.com>
2022-11-01 18:53:28 +0600

    Two Sum IV - Input is a BST

--- D_49.cpp
+++ D_49.cpp
@@ -2,47 +2,75 @@
 
 using namespace std;
 
-class ListNode
+class TreeNode
 {
 public:
   int val;
-  ListNode *next;
-  ListNode(int val)
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int val)
   {
     this->val = val;
-    next = NULL;
+    left = NULL;
+    right = NULL;
   }
 };
-
-void printList(ListNode *head)
-{
-  if (head == NULL)
-    return;
-  cout << head->val << " ";
-  printList(head->next);
-}
 
 class Solution
 {
 public:
-  string defangIPaddr(string address)
+  vector<int> nums;
+  void preOrderTraverse(TreeNode *root)
   {
-    string ans = "";
-    for (auto ch : address)
-    {
-      if (ch == '.')
-        ans += "[.]";
-      else
-        ans += ch;
-    }
-    return ans;
+    if (root == NULL)
+      return;
+    preOrderTraverse(root->left);
+    nums.push_back(root->val);
+    preOrderTraverse(root->right);
+  }
+
+  bool findTargetSum(int start, int end, int target)
+  {
+    bool result = false;
+    if (start >= end)
+      return result;
+    if (nums[end] < target / 2)
+      return result;
+    int res = nums[start] + nums[end];
+    if (res == target)
+      return true;
+    if (res > target)
+      return findTargetSum(start, end - 1, target);
+    if (res < target)
+      return findTargetSum(start + 1, end, target);
+    return result;
+  }
+
+  bool findTarget(TreeNode *root, int k)
+  {
+    nums.clear();
+    preOrderTraverse(root);
+    return findTargetSum(0, nums.size() - 1, k);
   }
 };
 
 int main()
 {
   Solution st;
-  string res = st.defangIPaddr("255.100.50.0");
+  TreeNode *root = new TreeNode(5);
+  TreeNode *node1 = new TreeNode(3);
+  TreeNode *node2 = new TreeNode(6);
+  TreeNode *node3 = new TreeNode(2);
+  TreeNode *node4 = new TreeNode(4);
+  TreeNode *node5 = new TreeNode(7);
+
+  root->left = node1;
+  root->right = node2;
+  node1->left = node3;
+  node1->right = node4;
+  node2->right = node5;
+
+  bool res = st.findTarget(root, 9);
   cout << res << endl;
 
   return 0;
