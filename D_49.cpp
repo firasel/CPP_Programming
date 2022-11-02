a8c4cb7e7826238c52aed2a2b6dee2ecc80d6995
firasel <mdrasel601390@gmail.com>
2022-11-02 11:20:29 +0600

    Final Value of Variable After Performing Operations

--- D_49.cpp
+++ D_49.cpp
@@ -2,53 +2,28 @@
 
 using namespace std;
 
-class TreeNode
-{
-public:
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int val)
-  {
-    this->val = val;
-    left = NULL;
-    right = NULL;
-  }
-};
-
 class Solution
 {
 public:
-  bool isValidBST(TreeNode *root, long low = LONG_MIN, long high = LONG_MAX)
+  int finalValueAfterOperations(vector<string> &operations)
   {
-    return !root || (root->val > low && root->val < high && isValidBST(root->left, low, root->val) && isValidBST(root->right, root->val, high));
+    int res = 0;
+    for (auto str : operations)
+    {
+      if (str == "++X" || str == "X++")
+        res++;
+      else
+        res--;
+    }
+    return res;
   }
 };
 
 int main()
 {
   Solution st;
-  TreeNode *root = new TreeNode(6);
-  TreeNode *node1 = new TreeNode(2);
-  TreeNode *node2 = new TreeNode(8);
-  TreeNode *node3 = new TreeNode(0);
-  TreeNode *node4 = new TreeNode(4);
-  TreeNode *node5 = new TreeNode(7);
-  TreeNode *node6 = new TreeNode(9);
-  TreeNode *node7 = new TreeNode(3);
-  TreeNode *node8 = new TreeNode(5);
-
-  root->left = node1;
-  root->right = node2;
-  node1->left = node3;
-  node1->right = node4;
-  node4->left = node7;
-  node4->right = node8;
-
-  node2->left = node5;
-  node2->right = node6;
-
-  bool res = st.isValidBST(root);
+  vector<string> operations = {"X++", "++X", "--X", "X--"};
+  int res = st.finalValueAfterOperations(operations);
   cout << res << endl;
 
   return 0;
