33679235eff53146d10be0c74f43c9c9732a7235
firasel <mdrasel601390@gmail.com>
2022-11-04 02:47:15 +0600

    3Sum solved

--- D_49.cpp
+++ D_49.cpp
@@ -4,24 +4,36 @@ using namespace std;
 class Solution
 {
 public:
-  int majorityElement(vector<int> &nums)
+  vector<vector<int>> threeSum(vector<int> &nums)
   {
     sort(nums.begin(), nums.end());
-    int ans = nums[nums.size() - 1];
-    int count = 1;
-    for (int i = 0; i < nums.size() - 1; i++)
+    set<vector<int>> res;
+    int size = nums.size(), sum, st, end;
+    for (int i = 0; i < size - 2; i++)
     {
-      if (nums[i] == nums[i + 1])
+      st = i + 1, end = size - 1;
+      while (st < end)
       {
-        count++;
-      }
-      else
-      {
-        if (count > nums.size() / 2)
-          ans = nums[i];
-        count = 0;
+        sum = nums[i] + nums[st] + nums[end];
+        if (sum > 0)
+          end--;
+        else if (sum < 0)
+          st++;
+        else
+        {
+          vector<int> temp = {nums[i], nums[st], nums[end]};
+          res.insert(temp);
+          while (st < end && nums[st] == temp[1])
+            st++;
+          while (st < end && nums[end] == temp[2])
+            end--;
+        }
       }
     }
+
+    vector<vector<int>> ans;
+    for (auto nums : res)
+      ans.push_back(nums);
     return ans;
   }
 };
@@ -29,7 +41,13 @@ public:
 int main()
 {
   Solution st;
-  vector<int> nums = {-1, 1, 1, 1, 2, 1};
-  cout << st.majorityElement(nums) << endl;
+  vector<int> tNums = {-1, 0, 1, 2, -1, -4};
+  vector<vector<int>> res = st.threeSum(tNums);
+  for (auto nums : res)
+  {
+    for (auto num : nums)
+      cout << num << " ";
+    cout << endl;
+  }
   return 0;
 }
