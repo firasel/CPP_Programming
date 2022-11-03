34db6a0ecd29b49137efed8c03fd77e9c98e6e6e
firasel <mdrasel601390@gmail.com>
2022-11-03 20:01:04 +0600

    Devu and his Brother trying to solve

--- D_49.cpp
+++ D_49.cpp
@@ -1,30 +1,29 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-class Solution
-{
-public:
-  int finalValueAfterOperations(vector<string> &operations)
-  {
-    int res = 0;
-    for (auto str : operations)
-    {
-      if (str == "++X" || str == "X++")
-        res++;
-      else
-        res--;
-    }
-    return res;
-  }
-};
 
 int main()
 {
-  Solution st;
-  vector<string> operations = {"X++", "++X", "--X", "X--"};
-  int res = st.finalValueAfterOperations(operations);
-  cout << res << endl;
-
+  int n, m;
+  cin >> n >> m;
+  vector<int> nums1(n);
+  vector<int> nums2(m);
+  for (int i = 0; i < n; i++)
+    cin >> nums1[i];
+  for (int i = 0; i < m; i++)
+    cin >> nums2[i];
+  sort(nums1.begin(), nums1.end());
+  sort(nums2.begin(), nums2.end());
+  int count = 0;
+  if (nums1[0] < nums2[m - 1])
+  {
+    int conNum = nums2[(m - 1) / 2];
+    for (int i = 0; i < n; i++)
+      count += abs(nums1[i] - conNum);
+    for (int i = 0; i < m; i++)
+      count += abs(nums2[i] - conNum);
+  }
+  cout << count << endl;
   return 0;
 }
