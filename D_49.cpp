9efc48564bf6dccda1784185d369f87c06c07e52
firasel <mdrasel601390@gmail.com>
2022-11-04 20:56:50 +0600

    Merge Intervals

--- D_49.cpp
+++ D_49.cpp
@@ -4,30 +4,37 @@ using namespace std;
 class Solution
 {
 public:
-  void sortColors(vector<int> &nums)
+  vector<vector<int>> merge(vector<vector<int>> &intervals)
   {
-    map<int, int> mapNum = {{0, 0}, {1, 0}, {2, 0}};
-    for (auto num : nums)
-      mapNum[num]++;
-    for (int i = 0; i < nums.size(); i++)
+    vector<vector<int>> res;
+    sort(intervals.begin(), intervals.end());
+    int size = intervals.size() - 1;
+    for (int i = 0; i <= size; i++)
     {
-      if (--mapNum[0] >= 0)
-        nums[i] = 0;
-      else if (--mapNum[1] >= 0)
-        nums[i] = 1;
-      else if (--mapNum[2] >= 0)
-        nums[i] = 2;
+      if (i == size)
+        res.push_back(intervals[i]);
+      else if (intervals[i][1] >= intervals[i + 1][0])
+      {
+        intervals[i + 1][0] = intervals[i][0];
+        intervals[i + 1][1] = max(intervals[i][1], intervals[i + 1][1]);
+      }
+      else
+        res.push_back({intervals[i][0], intervals[i][1]});
     }
+    return res;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums = {2, 0, 2, 1, 1, 0};
-  st.sortColors(nums);
-  for (auto num : nums)
-    cout << num << " ";
-  cout << endl;
+  vector<vector<int>> nums = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+  vector<vector<int>> res = st.merge(nums);
+  for (auto resNums : res)
+  {
+    for (auto num : resNums)
+      cout << num << " ";
+    cout << endl;
+  }
   return 0;
 }
