1e2886a3977c4b7c95a1155322bd7eca339e6a27
firasel <mdrasel601390@gmail.com>
2022-11-04 23:52:13 +0600

    Really Big Numbers

--- D_49.cpp
+++ D_49.cpp
@@ -1,52 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class MyHashMap
+int digit_sum(long long num)
 {
-public:
-  vector<int> data;
-  MyHashMap()
+  long long sum = 0;
+  while (num > 0)
   {
-    data.resize(1E6 + 1, -1);
+    sum += num % 10;
+    num /= 10;
   }
-
-  void put(int key, int value)
-  {
-    data[key] = value;
-  }
-
-  int get(int key)
-  {
-    return data[key];
-  }
-
-  void remove(int key) { data[key] = -1; }
-};
-
-class Solution
-{
-public:
-};
+  return sum;
+}
 
 int main()
 {
-  Solution st;
-
-  MyHashMap *obj = new MyHashMap();
-  obj->remove(14);
-  cout << obj->get(4) << endl;
-  obj->put(7, 3);
-  obj->put(11, 1);
-  obj->put(12, 1);
-  cout << obj->get(7) << endl;
-  obj->put(7, 4);
-  cout << obj->get(7) << endl;
-  obj->put(1, 19);
-  obj->put(0, 3);
-  obj->put(1, 8);
-  obj->put(2, 6);
-  cout << obj->get(0) << endl;
-  cout << obj->get(2) << endl;
-
+  long long n, s;
+  cin >> n >> s;
+  long long lo = 1, hi = n + 1;
+  while (lo < hi)
+  {
+    long long mid = lo + (hi - lo) / 2;
+    if (mid - digit_sum(mid) >= s)
+      hi = mid;
+    else
+      lo = mid + 1;
+  }
+  if (lo > n)
+    cout << 0 << endl;
+  else
+    cout << n - lo + 1 << endl;
   return 0;
 }
