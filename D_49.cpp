d0a615aa0decbe01d77b880e1abadf148ac7d8fc
firasel <mdrasel601390@gmail.com>
2022-11-04 18:38:42 +0600

    Sort colors

--- D_49.cpp
+++ D_49.cpp
@@ -4,50 +4,30 @@ using namespace std;
 class Solution
 {
 public:
-  vector<vector<int>> threeSum(vector<int> &nums)
+  void sortColors(vector<int> &nums)
   {
-    sort(nums.begin(), nums.end());
-    set<vector<int>> res;
-    int size = nums.size(), sum, st, end;
-    for (int i = 0; i < size - 2; i++)
+    map<int, int> mapNum = {{0, 0}, {1, 0}, {2, 0}};
+    for (auto num : nums)
+      mapNum[num]++;
+    for (int i = 0; i < nums.size(); i++)
     {
-      st = i + 1, end = size - 1;
-      while (st < end)
-      {
-        sum = nums[i] + nums[st] + nums[end];
-        if (sum > 0)
-          end--;
-        else if (sum < 0)
-          st++;
-        else
-        {
-          vector<int> temp = {nums[i], nums[st], nums[end]};
-          res.insert(temp);
-          while (st < end && nums[st] == temp[1])
-            st++;
-          while (st < end && nums[end] == temp[2])
-            end--;
-        }
-      }
+      if (--mapNum[0] >= 0)
+        nums[i] = 0;
+      else if (--mapNum[1] >= 0)
+        nums[i] = 1;
+      else if (--mapNum[2] >= 0)
+        nums[i] = 2;
     }
-
-    vector<vector<int>> ans;
-    for (auto nums : res)
-      ans.push_back(nums);
-    return ans;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> tNums = {-1, 0, 1, 2, -1, -4};
-  vector<vector<int>> res = st.threeSum(tNums);
-  for (auto nums : res)
-  {
-    for (auto num : nums)
-      cout << num << " ";
-    cout << endl;
-  }
+  vector<int> nums = {2, 0, 2, 1, 1, 0};
+  st.sortColors(nums);
+  for (auto num : nums)
+    cout << num << " ";
+  cout << endl;
   return 0;
 }
