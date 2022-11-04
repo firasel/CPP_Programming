03e841a09ed96273ce0e690a967412fd64414051
firasel <mdrasel601390@gmail.com>
2022-11-04 22:28:47 +0600

    Design HashMap

--- D_49.cpp
+++ D_49.cpp
@@ -1,40 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class MyHashMap
+{
+public:
+  vector<int> data;
+  MyHashMap()
+  {
+    data.resize(1E6 + 1, -1);
+  }
+
+  void put(int key, int value)
+  {
+    data[key] = value;
+  }
+
+  int get(int key)
+  {
+    return data[key];
+  }
+
+  void remove(int key) { data[key] = -1; }
+};
+
 class Solution
 {
 public:
-  vector<vector<int>> merge(vector<vector<int>> &intervals)
-  {
-    vector<vector<int>> res;
-    sort(intervals.begin(), intervals.end());
-    int size = intervals.size() - 1;
-    for (int i = 0; i <= size; i++)
-    {
-      if (i == size)
-        res.push_back(intervals[i]);
-      else if (intervals[i][1] >= intervals[i + 1][0])
-      {
-        intervals[i + 1][0] = intervals[i][0];
-        intervals[i + 1][1] = max(intervals[i][1], intervals[i + 1][1]);
-      }
-      else
-        res.push_back({intervals[i][0], intervals[i][1]});
-    }
-    return res;
-  }
 };
 
 int main()
 {
   Solution st;
-  vector<vector<int>> nums = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-  vector<vector<int>> res = st.merge(nums);
-  for (auto resNums : res)
-  {
-    for (auto num : resNums)
-      cout << num << " ";
-    cout << endl;
-  }
+
+  MyHashMap *obj = new MyHashMap();
+  obj->remove(14);
+  cout << obj->get(4) << endl;
+  obj->put(7, 3);
+  obj->put(11, 1);
+  obj->put(12, 1);
+  cout << obj->get(7) << endl;
+  obj->put(7, 4);
+  cout << obj->get(7) << endl;
+  obj->put(1, 19);
+  obj->put(0, 3);
+  obj->put(1, 8);
+  obj->put(2, 6);
+  cout << obj->get(0) << endl;
+  cout << obj->get(2) << endl;
+
   return 0;
 }
