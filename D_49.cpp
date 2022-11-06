a6f731bec8a68c69cfd9ecd41e4fd8e696b963c7
firasel <mdrasel601390@gmail.com>
2022-11-06 19:49:59 +0600

    Search a 2D Matrix II

--- D_49.cpp
+++ D_49.cpp
@@ -4,36 +4,37 @@ using namespace std;
 class Solution
 {
 public:
-  vector<int> applyOperations(vector<int> &nums)
+  bool searchMatrix(vector<vector<int>> &matrix, int target)
   {
-    int size = nums.size(), index = 0;
-    for (int i = 0; i < size - 1; i++)
+    int lo, hi, mid;
+    for (int i = 0; i < matrix.size(); i++)
     {
-      if (nums[i] == nums[i + 1])
+      if (matrix[i].front() <= target && matrix[i].back() >= target)
       {
-        nums[i] *= 2;
-        nums[i + 1] = 0;
+        lo = 0, hi = matrix[i].size() - 1;
+        while (lo <= hi)
+        {
+          mid = lo + (hi - lo) / 2;
+          if (matrix[i][mid] == target)
+            return true;
+          else if (matrix[i][mid] > target)
+            hi = mid - 1;
+          else
+            lo = mid + 1;
+        }
       }
+      if (matrix[i].front() > target)
+        return false;
     }
-
-    for (int i = 0; i < size; i++)
-      if (nums[i] != 0)
-        nums[index++] = nums[i];
-
-    while (index < size)
-      nums[index++] = 0;
-
-    return nums;
+    return false;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums = {1, 2, 2, 1, 1, 0};
-  vector<int> res = st.applyOperations(nums);
-  for (auto num : res)
-    cout << num << " ";
-  cout << endl;
+  vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+  bool res = st.searchMatrix(matrix, 5);
+  cout << res << endl;
   return 0;
 }
