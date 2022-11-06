b3a64e6e88da2e62fc318bad2db049aecd6c540d
firasel <mdrasel601390@gmail.com>
2022-11-06 10:15:31 +0600

    Apply operations to an array

--- D_49.cpp
+++ D_49.cpp
@@ -4,33 +4,36 @@ using namespace std;
 class Solution
 {
 public:
-  void rotate(vector<vector<int>> &matrix)
+  vector<int> applyOperations(vector<int> &nums)
   {
-    reverse(matrix.begin(), matrix.end());
-    int size = matrix.size();
-    for (int j = 0; j < size; j++)
+    int size = nums.size(), index = 0;
+    for (int i = 0; i < size - 1; i++)
     {
-      for (int i = j + 1; i < size; i++)
+      if (nums[i] == nums[i + 1])
       {
-        int temp = matrix[j][i];
-        matrix[j][i] = matrix[i][j];
-        matrix[i][j] = temp;
+        nums[i] *= 2;
+        nums[i + 1] = 0;
       }
     }
+
+    for (int i = 0; i < size; i++)
+      if (nums[i] != 0)
+        nums[index++] = nums[i];
+
+    while (index < size)
+      nums[index++] = 0;
+
+    return nums;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-  st.rotate(matrix);
-  for (auto nums : matrix)
-  {
-    for (auto num : nums)
-      cout << num << " ";
-    cout << endl;
-  }
+  vector<int> nums = {1, 2, 2, 1, 1, 0};
+  vector<int> res = st.applyOperations(nums);
+  for (auto num : res)
+    cout << num << " ";
   cout << endl;
   return 0;
 }
