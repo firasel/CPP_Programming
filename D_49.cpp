8909f63562cf950c2a92c32b1c6cf9ebacd5b3be
firasel <mdrasel601390@gmail.com>
2022-11-06 20:48:15 +0600

    Non-overlapping Intervals

--- D_49.cpp
+++ D_49.cpp
@@ -4,37 +4,33 @@ using namespace std;
 class Solution
 {
 public:
-  bool searchMatrix(vector<vector<int>> &matrix, int target)
+  int eraseOverlapIntervals(vector<vector<int>> &intervals)
   {
-    int lo, hi, mid;
-    for (int i = 0; i < matrix.size(); i++)
+    sort(intervals.begin(), intervals.end());
+    int count = 0, prev = intervals[0][1];
+    for (int i = 1; i < intervals.size(); i++)
     {
-      if (matrix[i].front() <= target && matrix[i].back() >= target)
+      if (prev > intervals[i][0])
       {
-        lo = 0, hi = matrix[i].size() - 1;
-        while (lo <= hi)
-        {
-          mid = lo + (hi - lo) / 2;
-          if (matrix[i][mid] == target)
-            return true;
-          else if (matrix[i][mid] > target)
-            hi = mid - 1;
-          else
-            lo = mid + 1;
-        }
+        count++;
+        prev = min(prev, intervals[i][1]);
       }
-      if (matrix[i].front() > target)
-        return false;
+      else
+        prev = intervals[i][1];
     }
-    return false;
+    return count;
   }
 };
 
 int main()
 {
   Solution st;
-  vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
-  bool res = st.searchMatrix(matrix, 5);
+  vector<vector<int>> intervals = {{1, 100}, {11, 22}, {1, 11}, {2, 12}};
+  vector<vector<int>> intervals2 = {
+      {-52, 31}, {-73, -26}, {82, 97}, {-65, -11}, {-62, -49}, {95, 99}, {58, 95}, {-31, 49}, {66, 98}, {-63, 2}, {30, 47}, {-40, -26}};
+  int res = st.eraseOverlapIntervals(intervals);
+  int res2 = st.eraseOverlapIntervals(intervals2);
   cout << res << endl;
+  cout << res2 << endl;
   return 0;
 }
