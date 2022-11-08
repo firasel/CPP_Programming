be744534f9d9e272669774a9f395c34dfaf95511
firasel <mdrasel601390@gmail.com>
2022-11-08 20:29:03 +0600

    Longest Palindrome

--- D_49.cpp
+++ D_49.cpp
@@ -5,63 +5,29 @@ using namespace std;
 class Solution
 {
 public:
-  string sumOfSingleNum(int num1 = 0, int num2 = 0, int carr = 0)
+  int longestPalindrome(string s)
   {
-    return to_string((num1 + num2 + carr));
-  }
-
-  int strToNum(string numStr)
-  {
-    if (numStr.size() == 0)
-      return 0;
-    int num = 0;
-    for (auto ch : numStr)
-      num *= 10, num += (ch - '0');
-    return num;
-  }
-
-  string addStrings(string num1, string num2)
-  {
-    string ans = "", temp;
-    int carr = 0, n1 = num1.size() - 1, n2 = num2.size() - 1;
-    while (n1 >= 0 && n2 >= 0)
+    int cnt = 0, temp = 0, size = s.size();
+    if (size < 1)
+      return cnt;
+    else if (size < 2)
+      return 1;
+    sort(s.begin(), s.end());
+    for (int i = 0; i < size; i++)
     {
-      temp = sumOfSingleNum(num1[n1] - '0', num2[n2] - '0', carr);
-      ans = temp.back() + ans;
-      temp.pop_back();
-      carr = strToNum(temp);
-      n1--;
-      n2--;
+      if (i != size - 1 && s[i] == s[i + 1])
+        i++, cnt += 2;
+      else if (temp == 0)
+        temp = 1;
     }
-
-    while (n1 >= 0)
-    {
-      temp = sumOfSingleNum(num1[n1] - '0', 0, carr);
-      ans = temp.back() + ans;
-      temp.pop_back();
-      carr = strToNum(temp);
-      n1--;
-    }
-
-    while (n2 >= 0)
-    {
-      temp = sumOfSingleNum(num2[n2] - '0', 0, carr);
-      ans = temp.back() + ans;
-      temp.pop_back();
-      carr = strToNum(temp);
-      n2--;
-    }
-
-    if (carr > 0)
-      ans = to_string(carr) + ans;
-    return ans;
+    return cnt + temp;
   }
 };
 
 int main()
 {
   Solution st;
-  string res = st.addStrings("3876620623801494171", "6529364523802684779");
+  int res = st.longestPalindrome("abccccdd");
   cout << res << endl;
 
   return 0;
