7b96b9e278f2a55de40ea1205ed49d537eae5940
firasel <mdrasel601390@gmail.com>
2022-11-11 20:06:18 +0600

    Repeated DNA Sequences

--- D_49.cpp
+++ D_49.cpp
@@ -5,31 +5,26 @@ using namespace std;
 class Solution
 {
 public:
-  string multiply(string num1, string num2)
+  vector<string> findRepeatedDnaSequences(string s)
   {
-    if (num1 == "0" || num2 == "0")
-      return "0";
-    int len1 = num1.length(), len2 = num2.length();
-    string ans(len1 + len2, '0');
-    for (int i = len1 - 1; i >= 0; i--)
-    {
-      for (int j = len2 - 1; j >= 0; j--)
-      {
-        int res = (ans[i + j + 1] - '0') + ((num1[i] - '0') * (num2[j] - '0'));
-        ans[i + j + 1] = res % 10 + '0';
-        ans[i + j] += res / 10;
-      }
-    }
-    if (ans[0] == '0')
-      return ans.substr(1);
-    return ans;
+    vector<string> res;
+    if (s.size() < 10)
+      return res;
+    unordered_map<string, int> mp;
+    for (int i = 0; i + 9 < s.size(); i++)
+      mp[s.substr(i, 10)]++;
+    for (auto subStr : mp)
+      if (subStr.second > 1)
+        res.push_back(subStr.first);
+    return res;
   }
 };
 
 int main()
 {
   Solution st;
-  string res = st.multiply("123", "456");
-  cout << res << endl;
+  vector<string> res = st.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+  for (auto str : res)
+    cout << str << endl;
   return 0;
 }
