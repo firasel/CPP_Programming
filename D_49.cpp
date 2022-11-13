d98c1b2d918c500d7218557ea89c7d507e2f1372
firasel <mdrasel601390@gmail.com>
2022-11-14 00:18:22 +0600

    Remove Duplicates from Sorted List II

--- D_49.cpp
+++ D_49.cpp
@@ -16,26 +16,6 @@ public:
   }
 };
 
-class Solution
-{
-public:
-  int subarrayLCM(vector<int> &nums, int k)
-  {
-    unsigned int ans = 0, size = nums.size();
-    for (int i = 0; i < size; i++)
-    {
-      unsigned int temp = nums[i];
-      for (int j = i; j < size; j++)
-      {
-        temp = lcm(temp, nums[j]);
-        if (temp == k)
-          ans++;
-      }
-    }
-    return ans;
-  }
-};
-
 void printLinkedList(ListNode *head)
 {
   if (head == NULL)
@@ -44,11 +24,76 @@ void printLinkedList(ListNode *head)
   printLinkedList(head->next);
 }
 
+class Solution
+{
+public:
+  ListNode *deleteDuplicates(ListNode *head)
+  {
+    if (head == NULL || head->next == NULL)
+      return head;
+    ListNode *prev = NULL, *curr = head, *next = head->next;
+    int dt;
+    while (curr != NULL)
+    {
+      if (next != NULL && curr->val == next->val)
+      {
+        ListNode *temp = next->next;
+        ListNode *del1 = curr;
+        ListNode *del2 = next;
+        if (prev != NULL)
+          prev->next = temp;
+        else
+          head = temp;
+        curr = temp;
+        next = temp != NULL ? temp->next : temp;
+        dt = del1->val;
+        delete del1;
+        delete del2;
+      }
+      else if (dt == curr->val)
+      {
+        ListNode *temp = curr->next;
+        ListNode *del1 = curr;
+        if (prev != NULL)
+          prev->next = temp;
+        else
+          head = temp;
+        curr = temp;
+        next = temp != NULL ? temp->next : temp;
+        delete del1;
+      }
+      else
+      {
+        prev = curr;
+        curr = next;
+        next = next != NULL ? next->next : next;
+      }
+    }
+    return head;
+  }
+};
+
 int main()
 {
   Solution st;
-  vector<int> nums = {3, 6, 2, 7, 1};
-  int res = st.subarrayLCM(nums, 6);
-  cout << res << endl;
+  ListNode *list_1 = new ListNode(1);
+  ListNode *l1_2 = new ListNode(2);
+  ListNode *l1_3 = new ListNode(3);
+  ListNode *l1_4 = new ListNode(3);
+  ListNode *l1_5 = new ListNode(4);
+  ListNode *l1_6 = new ListNode(4);
+  ListNode *l1_7 = new ListNode(5);
+
+  list_1->next = l1_2;
+  l1_2->next = l1_3;
+  l1_3->next = l1_4;
+  l1_4->next = l1_5;
+  l1_5->next = l1_6;
+  l1_6->next = l1_7;
+
+  ListNode *res = st.deleteDuplicates(list_1);
+  printLinkedList(res);
+  cout << endl;
+
   return 0;
 }
