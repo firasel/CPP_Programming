c9645fa7ba64c9b0346f367a32e9ac00ea32d284
firasel <mdrasel601390@gmail.com>
2022-11-14 00:19:42 +0600

    Intersection of Two Linked Lists

--- D_49.cpp
+++ D_49.cpp
@@ -27,73 +27,66 @@ void printLinkedList(ListNode *head)
 class Solution
 {
 public:
-  ListNode *deleteDuplicates(ListNode *head)
+  int listCheckAndCount(ListNode *head, int &len)
   {
-    if (head == NULL || head->next == NULL)
-      return head;
-    ListNode *prev = NULL, *curr = head, *next = head->next;
-    int dt;
-    while (curr != NULL)
+    if (!head->next)
+      return head->val;
+    len += 1;
+    return listCheckAndCount(head->next, len);
+  }
+
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+  {
+    int len1 = 0, len2 = 0;
+    if (listCheckAndCount(headA, len1) != listCheckAndCount(headB, len2))
+      return NULL;
+    while (len1 != len2)
     {
-      if (next != NULL && curr->val == next->val)
+      if (len1 > len2)
       {
-        ListNode *temp = next->next;
-        ListNode *del1 = curr;
-        ListNode *del2 = next;
-        if (prev != NULL)
-          prev->next = temp;
-        else
-          head = temp;
-        curr = temp;
-        next = temp != NULL ? temp->next : temp;
-        dt = del1->val;
-        delete del1;
-        delete del2;
+        headA = headA->next;
+        len1--;
       }
-      else if (dt == curr->val)
+      else if (len2 > len1)
       {
-        ListNode *temp = curr->next;
-        ListNode *del1 = curr;
-        if (prev != NULL)
-          prev->next = temp;
-        else
-          head = temp;
-        curr = temp;
-        next = temp != NULL ? temp->next : temp;
-        delete del1;
-      }
-      else
-      {
-        prev = curr;
-        curr = next;
-        next = next != NULL ? next->next : next;
+        headB = headB->next;
+        len2--;
       }
     }
-    return head;
+    while (headA != headB)
+    {
+      headA = headA->next;
+      headB = headB->next;
+    }
+    return headA;
   }
 };
 
 int main()
 {
   Solution st;
-  ListNode *list_1 = new ListNode(1);
-  ListNode *l1_2 = new ListNode(2);
-  ListNode *l1_3 = new ListNode(3);
-  ListNode *l1_4 = new ListNode(3);
-  ListNode *l1_5 = new ListNode(4);
-  ListNode *l1_6 = new ListNode(4);
-  ListNode *l1_7 = new ListNode(5);
+  ListNode *list_1 = new ListNode(4);
+  ListNode *l1_2 = new ListNode(1);
+
+  ListNode *list_2 = new ListNode(5);
+  ListNode *l2_2 = new ListNode(6);
+  ListNode *l2_3 = new ListNode(1);
+
+  ListNode *l_3 = new ListNode(8);
+  ListNode *l_4 = new ListNode(4);
+  ListNode *l_5 = new ListNode(5);
+
+  l_3->next = l_4;
+  l_4->next = l_5;
 
   list_1->next = l1_2;
-  l1_2->next = l1_3;
-  l1_3->next = l1_4;
-  l1_4->next = l1_5;
-  l1_5->next = l1_6;
-  l1_6->next = l1_7;
+  l1_2->next = l_3;
 
-  ListNode *res = st.deleteDuplicates(list_1);
-  printLinkedList(res);
-  cout << endl;
+  list_2->next = l2_2;
+  l2_2->next = l2_3;
+  l2_3->next = l_3;
 
+  ListNode *res = st.getIntersectionNode(list_1, list_2);
+  cout << (res ? res->val : 0) << endl;
   return 0;
 }
