72b0bf36ec303d3bfdfd0a410b00b68d3e518ceb
firasel <mdrasel601390@gmail.com>
2022-11-14 00:42:08 +0600

    Reverse Words in a String

--- D_49.cpp
+++ D_49.cpp
@@ -27,66 +27,31 @@ void printLinkedList(ListNode *head)
 class Solution
 {
 public:
-  int listCheckAndCount(ListNode *head, int &len)
+  string reverseWords(string s)
   {
-    if (!head->next)
-      return head->val;
-    len += 1;
-    return listCheckAndCount(head->next, len);
-  }
-
-  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-  {
-    int len1 = 0, len2 = 0;
-    if (listCheckAndCount(headA, len1) != listCheckAndCount(headB, len2))
-      return NULL;
-    while (len1 != len2)
+    string ans = "", temp = " ";
+    for (auto ch : s)
     {
-      if (len1 > len2)
+      if (ch != ' ')
+        temp += ch;
+      else if (ch == ' ' && temp != "" && temp != " ")
       {
-        headA = headA->next;
-        len1--;
-      }
-      else if (len2 > len1)
-      {
-        headB = headB->next;
-        len2--;
+        ans = temp + ans;
+        temp = " ";
       }
     }
-    while (headA != headB)
-    {
-      headA = headA->next;
-      headB = headB->next;
-    }
-    return headA;
+    if (temp != "")
+      ans = temp + ans;
+    while (ans[0] == ' ')
+      ans.erase(0, 1);
+    return ans;
   }
 };
 
 int main()
 {
   Solution st;
-  ListNode *list_1 = new ListNode(4);
-  ListNode *l1_2 = new ListNode(1);
-
-  ListNode *list_2 = new ListNode(5);
-  ListNode *l2_2 = new ListNode(6);
-  ListNode *l2_3 = new ListNode(1);
-
-  ListNode *l_3 = new ListNode(8);
-  ListNode *l_4 = new ListNode(4);
-  ListNode *l_5 = new ListNode(5);
-
-  l_3->next = l_4;
-  l_4->next = l_5;
-
-  list_1->next = l1_2;
-  l1_2->next = l_3;
-
-  list_2->next = l2_2;
-  l2_2->next = l2_3;
-  l2_3->next = l_3;
-
-  ListNode *res = st.getIntersectionNode(list_1, list_2);
-  cout << (res ? res->val : 0) << endl;
+  string res = st.reverseWords("the sky is blue");
+  cout << res << endl;
   return 0;
 }
