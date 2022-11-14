c65772c2bde58fb82549d2f075701631b38c4fe6
firasel <mdrasel601390@gmail.com>
2022-11-14 20:39:39 +0600

    Design Linked List

--- D_49.cpp
+++ D_49.cpp
@@ -49,6 +49,97 @@ public:
   }
 };
 
+class MyLinkedList
+{
+  MyLinkedList *head;
+
+public:
+  int val;
+  MyLinkedList *next;
+  MyLinkedList()
+  {
+    head = NULL;
+  }
+
+  MyLinkedList(int val)
+  {
+    this->val = val;
+    next = NULL;
+  }
+
+  int get(int index)
+  {
+    MyLinkedList *temp = head;
+    int cnt = 0;
+    while (index != cnt && temp)
+      temp = temp->next, cnt++;
+    if (temp)
+      return temp->val;
+    return -1;
+  }
+
+  void addAtHead(int val)
+  {
+    MyLinkedList *newNode = new MyLinkedList(val);
+    newNode->next = head;
+    head = newNode;
+  }
+
+  void addAtTail(int val)
+  {
+    if (head == NULL)
+    {
+      addAtHead(val);
+      return;
+    }
+    MyLinkedList *temp = head;
+    while (temp->next != NULL)
+      temp = temp->next;
+    MyLinkedList *newNode = new MyLinkedList(val);
+    temp->next = newNode;
+  }
+
+  void addAtIndex(int index, int val)
+  {
+    if (index == 0)
+    {
+      addAtHead(val);
+      return;
+    }
+    MyLinkedList *temp = head;
+    int cnt = 1;
+    while (cnt != index && temp)
+      temp = temp->next, cnt++;
+    if (temp == NULL)
+      return;
+    MyLinkedList *newNode = new MyLinkedList(val);
+    newNode->next = temp->next;
+    temp->next = newNode;
+  }
+
+  void deleteAtIndex(int index)
+  {
+    if (index == 0)
+    {
+      MyLinkedList *del = head;
+      head = head->next;
+      delete del;
+      return;
+    }
+    MyLinkedList *temp = head;
+    int cnt = 1;
+    while (cnt != index && temp)
+      temp = temp->next, cnt++;
+    if (temp == NULL)
+      return;
+    MyLinkedList *del = temp->next;
+    if (del == NULL)
+      return;
+    temp->next = temp->next->next;
+    delete del;
+  }
+};
+
 int main()
 {
   Solution st;
