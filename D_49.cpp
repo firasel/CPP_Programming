a695706a622134c2ed98571c3b409aed6a2d78c4
firasel <mdrasel601390@gmail.com>
2022-11-18 02:33:06 +0600

    Construct Binary Tree from Preorder and Inorder Traversal

--- D_49.cpp
+++ D_49.cpp
@@ -4,33 +4,104 @@
 
 using namespace std;
 
+class TreeNode
+{
+public:
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int val)
+  {
+    this->val = val;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+void printTree(TreeNode *root, string &str)
+{
+  if (root == NULL)
+  {
+    str += "NULL_";
+    return;
+  }
+  cout << root->val << " ";
+  str += to_string(root->val) + '_';
+  printTree(root->left, str);
+  printTree(root->right, str);
+};
+
 class Solution
 {
 public:
-  int findTheWinner(int n, int k)
+  void printVector(vector<int> nums)
   {
-    int cnt = 1, temp;
-    queue<int> nums;
-    for (int i = 1; i <= n; i++)
-      nums.push(i);
-    while (nums.size() > 1)
+    cout << "Vect: ";
+    for (auto num : nums)
+      cout
+          << num << " ";
+    cout << endl;
+  }
+
+  int searchVector(vector<int> &nums, int val)
+  {
+    for (int i = 0; i < nums.size(); i++)
     {
-      temp = nums.front();
-      nums.pop();
-      if (cnt == k)
-        cnt = 0;
-      else
-        nums.push(temp);
-      cnt++;
+      if (nums[i] == val)
+        return i;
     }
-    return nums.front();
+    return -1;
+  }
+
+  void copyVector(vector<int> &nums, vector<int> &res, int st, int end)
+  {
+    res.clear();
+    for (st; st < end; st++)
+    {
+      res.push_back(nums[st]);
+    }
+  }
+
+  TreeNode *createBST(vector<int> &preorder, vector<int> &inorder)
+  {
+    if (inorder.size() == 0 || preorder.size() == 0)
+      return NULL;
+    int pos = searchVector(inorder, preorder[0]);
+    if (pos == -1)
+      return NULL;
+    TreeNode *root = new TreeNode(preorder[0]);
+    preorder.erase(preorder.begin());
+    vector<int> temp;
+    copyVector(inorder, temp, 0, pos);
+    if (!temp.empty())
+      root->left = createBST(preorder, temp);
+
+    copyVector(inorder, temp, pos + 1, inorder.size());
+    if (!temp.empty())
+      root->right = createBST(preorder, temp);
+
+    return root;
+  }
+
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+  {
+    TreeNode *root;
+    root = createBST(preorder, inorder);
+    return root;
   }
 };
 
 int main()
 {
   Solution st;
-  int res = st.findTheWinner(5, 2);
-  cout << res << endl;
+  vector<int> nums1 = {3, 9, 20, 15, 7};
+  vector<int> nums2 = {9, 3, 15, 20, 7};
+  TreeNode *res = st.buildTree(nums1, nums2);
+  cout << endl;
+  string treeStr = "";
+  printTree(res, treeStr);
+  cout << endl;
+  cout << endl
+       << treeStr << endl;
   return 0;
 }
