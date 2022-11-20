4d708c9d696d781b59d5cee60175c6c8cf201ade
firasel <mdrasel601390@gmail.com>
2022-11-20 18:39:29 +0600

    Lowest Common Ancestor of a Binary Tree

--- D_49.cpp
+++ D_49.cpp
@@ -30,33 +30,41 @@ void printTree(TreeNode *root)
 class Solution
 {
 public:
-  vector<int> sortedSquares(vector<int> &nums)
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
   {
-    int size = nums.size();
-    for (int i = 0; i < size; i++)
-      if (nums[i] < 0)
-        nums[i] = abs(nums[i]);
-    sort(nums.begin(), nums.end());
-    for (int i = 0; i < size; i++)
-      nums[i] = nums[i] * nums[i];
-    return nums;
+    if (!root || root == p || root == q)
+      return root;
+    TreeNode *root1 = lowestCommonAncestor(root->left, p, q);
+    TreeNode *root2 = lowestCommonAncestor(root->right, p, q);
+    if (root1 && root2)
+      return root;
+    return root1 ? root1 : root2;
   }
 };
 
 int main()
 {
   Solution st;
-  // TreeNode *root = new TreeNode(3);
-  // TreeNode *n2 = new TreeNode(1);
-  // TreeNode *n3 = new TreeNode(4);
-  // TreeNode *n4 = new TreeNode(2);
+  TreeNode *root = new TreeNode(3);
+  TreeNode *n2 = new TreeNode(5);
+  TreeNode *n3 = new TreeNode(1);
+  TreeNode *n4 = new TreeNode(6);
+  TreeNode *n5 = new TreeNode(2);
+  TreeNode *n6 = new TreeNode(0);
+  TreeNode *n7 = new TreeNode(8);
+  TreeNode *n8 = new TreeNode(7);
+  TreeNode *n9 = new TreeNode(4);
 
-  // root->left = n2;
-  // root->right = n3;
-  // n2->right = n4;
-  vector<int> nums = {-7, -3, 2, 3, 11};
-  vector<int> res = st.sortedSquares(nums);
-  for (auto num : res)
-    cout << num << endl;
+  root->left = n2;
+  root->right = n3;
+  n2->left = n4;
+  n2->right = n5;
+  n3->left = n6;
+  n3->right = n7;
+  n5->left = n8;
+  n5->right = n9;
+
+  TreeNode *res = st.lowestCommonAncestor(root, n2, n3);
+  cout << res->val << endl;
   return 0;
 }
