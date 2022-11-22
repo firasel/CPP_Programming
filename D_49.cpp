414dd8d33f39743d2079bc25e5416060442d1938
firasel <mdrasel601390@gmail.com>
2022-11-22 19:33:47 +0600

    Kth Largest Element in an Array

--- D_49.cpp
+++ D_49.cpp
@@ -30,15 +30,10 @@ void printTree(TreeNode *root)
 class Solution
 {
 public:
-  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+  int findKthLargest(vector<int> &nums, int k)
   {
-    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-    sort(nums1.begin(), nums1.end());
-    int size = nums1.size();
-    cout << fixed << setprecision(5);
-    if (size % 2 == 0)
-      return ((double)nums1[(size / 2) - 1] + (double)nums1[size / 2]) / 2;
-    return (double)nums1[size / 2];
+    sort(nums.begin(), nums.end());
+    return nums[nums.size() - k];
   }
 };
 
@@ -46,9 +41,9 @@ int main()
 {
   Solution st;
 
-  vector<int> nums1 = {1, 2};
+  vector<int> nums1 = {3, 2, 1, 5, 6, 4};
   vector<int> nums2 = {3, 4};
-  double res = st.findMedianSortedArrays(nums1, nums2);
+  int res = st.findKthLargest(nums1, 2);
   cout << res << endl;
   return 0;
 }
