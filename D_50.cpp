0ba4a237df8b00df590cbbe5118449730b8734ad
firasel <mdrasel601390@gmail.com>
2022-11-24 15:20:33 +0600

    Divisors

--- D_50.cpp
+++ D_50.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -8,15 +8,26 @@ int main()
   cin >> n;
   while (n--)
   {
-    string str, s;
-    cin >> str;
-    for (int i = 0; i < 20; i++)
-      s += "Yes";
-    if (s.find(str, 0) == string::npos)
-      cout << "NO" << endl;
-    else
-      cout << "YES" << endl;
-  }
+    int l, r, max = 0, num;
+    cin >> l >> r;
 
+    for (int i = l; i <= r; i++)
+    {
+      int cnt = 0, lim = sqrt(i);
+      for (int j = 1; j <= lim; j++)
+      {
+        if (j * j == i)
+          cnt++;
+        else if (i % j == 0)
+          cnt += 2;
+      }
+      if (max < cnt)
+      {
+        max = cnt;
+        num = i;
+      }
+    }
+    cout << "Between " << l << " and " << r << ", " << num << " has a maximum of " << max << " divisors." << endl;
+  }
   return 0;
 }
