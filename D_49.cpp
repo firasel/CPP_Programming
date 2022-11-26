9c787278360d38651074d6cc9d519bb281ed34e9
firasel <mdrasel601390@gmail.com>
2022-11-26 21:14:18 +0600

    Minimum Cuts to Divide a Circle

--- D_49.cpp
+++ D_49.cpp
@@ -30,31 +30,13 @@ void printTree(TreeNode *root)
 class Solution
 {
 public:
-  bool isValidTriangle(int &a, int &b, int &c)
+  int numberOfCuts(int n)
   {
-    if (a + b > c && a + c > b && b + c > a)
-      return true;
-    return false;
-  }
-  int triangleNumber(vector<int> &nums)
-  {
-    int size = nums.size(), cnt = 0;
-    if (size < 3)
-      return cnt;
-    size -= 2;
-    for (int i = 0; i < size; i++)
-    {
-      if (isValidTriangle(nums[i], nums[i + 1], nums[i + 2]))
-        cnt++;
-    }
-    do
-    {
-      for (auto num : nums)
-        cout << num << " ";
-      cout << endl;
-    } while (next_permutation(nums.begin(), nums.end()));
-
-    return cnt;
+    if (n < 2)
+      return 0;
+    if (n % 2 == 0)
+      return n / 2;
+    return n;
   }
 };
 
@@ -62,7 +44,7 @@ int main()
 {
   Solution st;
   vector<int> nums = {2, 2, 3, 4};
-  int res = st.triangleNumber(nums);
+  int res = st.numberOfCuts(6);
   cout << res << endl;
   return 0;
 }
