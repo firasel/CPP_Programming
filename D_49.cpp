2be8080db145a415977fbaec62d4731c620e935b
firasel <mdrasel601390@gmail.com>
2022-11-26 21:48:21 +0600

    Difference Between Ones and Zeros in Row and Column - TLE

--- D_49.cpp
+++ D_49.cpp
@@ -38,13 +38,45 @@ public:
       return n / 2;
     return n;
   }
+  vector<vector<int>> onesMinusZeros(vector<vector<int>> &grid)
+  {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> ans(m, vector<int>(n));
+
+    for (int i = 0; i < m; i++)
+    {
+      int oneRow = 0, zeroRow = 0;
+      for (int j = 0; j < n; j++)
+        if (grid[i][j] == 1)
+          oneRow++;
+        else
+          zeroRow++;
+      for (int j = 0; j < n; j++)
+      {
+        int oneCol = 0, zeroCol = 0;
+        for (int k = 0; k < m; k++)
+          if (grid[k][j] == 1)
+            oneCol++;
+          else
+            zeroCol++;
+
+        ans[i][j] = oneRow + oneCol - zeroRow - zeroCol;
+      }
+    }
+    return ans;
+  }
 };
 
 int main()
 {
   Solution st;
-  vector<int> nums = {2, 2, 3, 4};
-  int res = st.numberOfCuts(6);
-  cout << res << endl;
+  vector<vector<int>> nums = {{0, 1, 1}, {1, 0, 1}, {0, 0, 1}};
+  vector<vector<int>> res = st.onesMinusZeros(nums);
+  for (auto nums : res)
+  {
+    for (auto num : nums)
+      cout << num << " ";
+    cout << endl;
+  }
   return 0;
 }
