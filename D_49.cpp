88dc0c8c23404c79b72e23101005f548bcfdce2a
firasel <mdrasel601390@gmail.com>
2022-12-04 09:17:18 +0600

    Divide Players Into Teams of Equal Skill

--- D_49.cpp
+++ D_49.cpp
@@ -30,45 +30,27 @@ void printTree(TreeNode *root)
 class Solution
 {
 public:
-  void removeDupWord(string &str, vector<string> &words)
+  long long dividePlayers(vector<int> &skill)
   {
-    string word = "";
-    for (auto x : str)
+    int size = skill.size();
+    long long temp = INT_MIN, sum = 0;
+    sort(skill.begin(), skill.end());
+    for (int i = 0; i < size / 2; i++)
     {
-      if (x == ' ')
-      {
-        if (word != "")
-          words.push_back(word);
-        word = "";
-      }
-      else
-        word = word + x;
+      if (temp != INT_MIN && temp != skill[i] + skill[size - 1 - i])
+        return -1;
+      temp = skill[i] + skill[size - 1 - i];
+      sum += (skill[i] * skill[size - 1 - i]);
     }
-    if (word != "")
-      words.push_back(word);
-  }
-
-  bool isCircularSentence(string sentence)
-  {
-    vector<string> words;
-    removeDupWord(sentence, words);
-    int size = words.size();
-    if (size > 1)
-    {
-      for (int i = 0; i < size - 1; i++)
-      {
-        if (words[i].back() != words[i + 1].front())
-          return false;
-      }
-    }
-    return words[0].front() == words[size - 1].back();
+    return sum;
   }
 };
 
 int main()
 {
   Solution st;
-  bool res = st.isCircularSentence("leetcode exercises sound delightful");
+  vector<int> nums = {1, 1, 2, 3};
+  long long res = st.dividePlayers(nums);
   cout << res << endl;
   return 0;
 }
