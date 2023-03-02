97d09c497dc530b36ab2a94a742f800763c9001a
firasel <mdrasel601390@gmail.com>
2023-03-03 00:24:20 +0600

    Is It a Cat - improve solution

--- cat.cpp
+++ cat.cpp
@@ -1,30 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool m = false, e = false, o = false, w = false;
-
 int convert(char ch)
 {
   if (ch == 'M' || ch == 'm')
-  {
-    m = true;
     return 1;
-  }
   else if (ch == 'E' || ch == 'e')
-  {
-    e = true;
     return 2;
-  }
   else if (ch == 'O' || ch == 'o')
-  {
-    o = true;
     return 3;
-  }
   else if (ch == 'W' || ch == 'w')
-  {
-    w = true;
     return 4;
-  }
   return -1;
 }
 // meow meow
@@ -38,26 +24,26 @@ int main()
     string str;
     cin >> str;
 
-    int prev = 0, flag = 0;
+    int prev = 0, flag = 0, cnt = 0;
+
     for (int i = 0; i < l; i++)
     {
       int temp = convert(str[i]);
+      if (prev == temp)
+        continue;
       if (temp == -1 || prev > temp)
       {
         flag = 1;
         break;
       }
       prev = temp;
+      cnt++;
     }
-    if (m && e && o && w && flag == 0)
+
+    if (cnt == 4 && !flag)
       cout << "YES" << endl;
     else
       cout << "NO" << endl;
-
-    m = false;
-    e = false;
-    o = false;
-    w = false;
   }
   return 0;
 }
